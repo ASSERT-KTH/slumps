7af64b534689d8a687cf2c9aef7c7e8eff073bf3
Jian GU <ycaxgjd@gmail.com>
2019-09-25 21:44:24 +0200

    Update run.sh

--- utils/souper2wasm/run.sh
+++ utils/souper2wasm/run.sh
@@ -4,4 +4,6 @@ echo "okay"
 
 echo "llc ... \c"
 llc -mtriple=wasm32-unknown-unknown -O3 -filetype=obj demo/infer.ll -o demo/infer.o
+# test this command
+# llc -march=wasm32 -filetype=obj demo/infer.ll
 echo "okay"
