2d01622f361fe8f97537aa3c8db136b5cab17cba
Jian GU <ycaxgjd@gmail.com>
2019-09-26 12:25:42 +0200

    fix the validity issue
    improve scripts

--- utils/souper2wasm/run.sh
+++ utils/souper2wasm/run.sh
@@ -1,9 +1,9 @@
+#!/bin/sh
+
 echo "py ... \c"
 python3 souper2llvm.py demo/infer.opt
 echo "okay"
 
 echo "llc ... \c"
-llc -mtriple=wasm32-unknown-unknown -O3 -filetype=obj demo/infer.ll -o demo/infer.o
-# test this command
-# llc -march=wasm32 -filetype=obj demo/infer.ll
+llc -march=wasm32 -filetype=obj demo/infer.ll
 echo "okay"
