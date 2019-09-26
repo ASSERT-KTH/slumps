8567dd5214bf7b9216f4c5593849168b290ea4a3
Jian GU <ycaxgjd@gmail.com>
2019-09-26 12:25:42 +0200

    fix the validity issue
    improve scripts

--- utils/souper_candidates/run.sh
+++ utils/souper_candidates/run.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 # we use infer.ll as one example
 echo "preparing \c"
 llvm-as demo/infer.ll -o demo/infer.bc
@@ -5,7 +7,13 @@ echo "okay"
 
 # $ /path/to/souper -z3-path=/usr/bin/z3 /path/to/file.bc
 echo "souper ... \c"
-./souper demo/infer.bc > demo/infer.candidates
+if [ "$(uname)"=="Darwin" ]; then
+	# for macOS
+	./souper demo/infer.bc -z3-path=/usr/local/Cellar/z3/4.8.6/bin/z3 > demo/infer.candidates
+else
+	# for ubuntu
+	./souper demo/infer.bc -z3-path=/usr/bin/z3 > demo/infer.candidates
+fi
 echo "okay"
 
 echo "py ... \c"
