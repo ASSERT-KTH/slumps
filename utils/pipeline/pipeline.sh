786e29e3b4abb1b7a2020c4dcdd3540685e730b5
Jian GU <ycaxgjd@gmail.com>
2019-10-08 09:07:01 +0200

    improvements

--- utils/pipeline/pipeline.sh
+++ utils/pipeline/pipeline.sh
@@ -8,36 +8,43 @@ ext=$(echo $1 | sed 's/^.*\.//')
 if [ "${ext}" == "wast" ]; then
   echo "### step1 wast2wasm \c"
   ../../wabt/bin/wat2wasm ${name}.wast -o ${name}.wasm
-  echo "okay"
-
-  echo "### step2 wasm2opt \c"
-  ../../binaryen/bin/wasm-opt ${name}.wasm --flatten --souperify-single-use > ${name}.opt
+  ext='wasm'
   echo "okay"
 fi
 
 if [ "${ext}" == "wasm" ]; then
   echo "### step2 wasm2opt \c"
   ../../binaryen/bin/wasm-opt ${name}.wasm --flatten --souperify-single-use > ${name}.opt
+  ext='opt'
   echo "okay"
 fi
 
-echo "### step3 opt2ll \c"
-python souper2llvm.py ${name}.opt > ${name}.ll
-# ../../souper/build/souper2llvm ${name}.opt > ${name}.ll # it seems not so good
-echo "okay"
+if [ "${ext}" == "opt" ]; then
+  echo "### step3 opt2ll \c"
+  python souper2llvm.py ${name}.opt > ${name}.ll
+  # ../../souper/build/souper2llvm ${name}.opt > ${name}.ll
+  ext='ll'
+  echo "okay"
+fi
 
-echo "### step4 ll2bc \c"
-llvm-as ${name}.ll
-echo "okay"
+if [ "${ext}" == "ll" ]; then
+  echo "### step4 ll2bc \c"
+  llvm-as ${name}.ll
+  ext='bc'
+  echo "okay"
+fi
 
-echo "### step5 bc2opt2ll \c"
-sh bc2opt2ll.sh ${name}.bc ../../souper/build/souper
-echo "okay"
+if [ "${ext}" == "bc" ]; then
+  echo "### step5 bc2opt2ll \c"
+  sh bc2opt2ll.sh ${name}.bc ../../souper/build/souper
+  ext='ll'
+  echo "okay"
+fi
 
-echo "### extra ll2s \c"
-llc -march=wasm32 -filetype=asm ${name}.ll
-echo "okay"
+# echo "### extra ll2s \c"
+# llc -march=wasm32 -filetype=asm ${name}.ll
+# echo "okay"
 
-echo "### extra ll2o \c"
-llc -march=wasm32 -filetype=obj ${name}.ll
-echo "okay"
+# echo "### extra ll2o \c"
+# llc -march=wasm32 -filetype=obj ${name}.ll
+# echo "okay"
