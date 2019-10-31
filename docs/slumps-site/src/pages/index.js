25b85110257568ad4feca864337b5ffec1855a2a
Javier Cabrera <javierca@kth.se>
2019-10-31 11:23:27 +0100

    Adding antd template

--- docs/slumps-site/src/pages/index.js
+++ docs/slumps-site/src/pages/index.js
@@ -1,5 +1,12 @@
 import React from "react"
 
+import { Layout, Menu, Breadcrumb, Button} from 'antd';
+
+import "../assets/layout.scss"
+
+
+const { Header, Content, Footer } = Layout;
+
 
 const IndexPage = ({
   data: { allMarkdownRemark: { edges } }
@@ -7,8 +14,33 @@ const IndexPage = ({
   
   const Posts = edges
     .filter(edge => !!edge.node.frontmatter.date) // You can filter your posts based on some criteria
-    .map(edge => <li key={edge.node.id}>post</li>)
-  return <ul>{Posts}</ul>
+    .map(edge => <li key={edge.node.id}><Button type="primary">Button</Button>
+    </li>)
+  return (<Layout>
+    <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
+      <div className="logo" />
+      <Menu
+        theme="dark"
+        mode="horizontal"
+        defaultSelectedKeys={['2']}
+        style={{ lineHeight: '64px' }}
+      >
+        <Menu.Item key="1">nav 1</Menu.Item>
+        <Menu.Item key="2">nav 2</Menu.Item>
+        <Menu.Item key="3">nav 3</Menu.Item>
+      </Menu>
+    </Header>
+    <Content style={{ padding: '0 50px', marginTop: 64 }}>
+      <Breadcrumb style={{ margin: '16px 0' }}>
+        <Breadcrumb.Item>Home</Breadcrumb.Item>
+        <Breadcrumb.Item>List</Breadcrumb.Item>
+        <Breadcrumb.Item>App</Breadcrumb.Item>
+      </Breadcrumb>
+      <div style={{ background: '#fff', padding: 24, minHeight: 380 }}>Content</div>
+      
+    </Content>
+    <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+  </Layout>)
 
 }
 
