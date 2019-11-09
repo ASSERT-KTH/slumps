eb56e4a77cab74b91bd9497514a2909d0cf3f3c7
Jian GU <ycaxgjd@gmail.com>
2019-11-09 21:55:08 +0100

    improve pipeline

--- utils/pipeline/python/pipeline.py
+++ utils/pipeline/python/pipeline.py
@@ -1,13 +1,13 @@
-import argparse
-from subprocess import Popen, PIPE
 import os
 import sys
+from subprocess import Popen, PIPE
 
 BASE_DIR = os.path.dirname(
     os.path.dirname(
         os.path.dirname(
             os.path.dirname(
                 os.path.abspath(__file__)))))
+
 
 class bcolors:
     HEADER = '\033[95m'
@@ -19,179 +19,181 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
+
 class Alias:
-    
-    clang = "%s/souper/third_party/llvm/Release/bin/clang"%(BASE_DIR,)
-    opt = "%s/souper/third_party/llvm/Release/bin/opt"%(BASE_DIR,)
-    llc = "%s/souper/third_party/llvm/Release/bin/llc"%(BASE_DIR,)
-    lli = "%s/souper/third_party/llvm/Release/bin/lli"%(BASE_DIR,)
-    llvm_as = "%s/souper/third_party/llvm/Release/bin/llvm-as"%(BASE_DIR,)
-    souper = "%s/souper/build/souper"%(BASE_DIR,)
-    souper_check = "%s/souper/build/souper-check"%(BASE_DIR,)
-    souper2llvm = "%s/souper/build/souper2llvm"%(BASE_DIR,)
-    # libsouperPass_so = "../../souper/build/libsouperPass.so"
-    z3 = "%s/souper/third_party/z3/build/z3"%(BASE_DIR,)
+    clang = "%s/souper/third_party/llvm/Release/bin/clang" % (BASE_DIR,)
+    opt = "%s/souper/third_party/llvm/Release/bin/opt" % (BASE_DIR,)
+    llc = "%s/souper/third_party/llvm/Release/bin/llc" % (BASE_DIR,)
+    lli = "%s/souper/third_party/llvm/Release/bin/lli" % (BASE_DIR,)
+    llvm_as = "%s/souper/third_party/llvm/Release/bin/llvm-as" % (BASE_DIR,)
+    souper = "%s/souper/build/souper" % (BASE_DIR,)
+    souper_check = "%s/souper/build/souper-check" % (BASE_DIR,)
+    souper2llvm = "%s/souper/build/souper2llvm" % (BASE_DIR,)
+    z3 = "%s/souper/third_party/z3/build/z3" % (BASE_DIR,)
+
 
 class ExternalStage(object):
-    
     def __init__(self):
         self.name = "unknown"
-        self.path_to_executable = "unknown"
+        self.executable = "unknown"
+        self.params = None
 
-    def processInner(self, std):
-        return b"Ops...Not implemented"
+    def __call__(self, std_in=None):
+        p = Popen([self.executable] + self.params, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+        if std_in is not None:
+            p.stdin.write(std_in)
 
-    def do(self,args=[], stdin = None): # stdin byte stream
+        print("%sStage ->%s %s %s" % (bcolors.OKGREEN, bcolors.ENDC, self.executable, self.name))
+        std_out, std_err = p.communicate()
 
-        p = Popen([self.path_to_executable] + args, stdout=PIPE, stderr=PIPE, stdin=PIPE)
-        
+        if p.returncode != 0 or std_err:
+            raise Exception("Error on stage: %s. %s" % (self.name, str(std_err)))
 
-        if stdin is not None:
-            p.stdin.write(stdin)
+        message = self.check()
+        print("\t%s%s%s'" % (bcolors.WARNING, message, bcolors.ENDC))
 
-        print("%sStage ->%s %s %s"%(bcolors.OKGREEN, bcolors.ENDC, self.path_to_executable, self.name))
-        std,err = p.communicate()
+    def check(self):
+        # not implemented
+        return True
 
-        rc = p.returncode
-
-        if rc != 0 or err:
-            raise Exception("Error on stage: %s. %s"%(self.name, str(err)))
-
-        # Specific implementation process over the std out
-        res = self.processInner(std)
-
-        print("\t%s%s%s'"%(bcolors.WARNING, res, bcolors.ENDC))
-        return res
 
 class CToLLStage(ExternalStage):
-
-
-    def __init__(self):
-        self.path_to_executable = Alias.clang
+    def __init__(self, filename):
+        super().__init__()
         self.name = "C to LLVM IR"
-    
-    def do(self, args=[], std = None): # f -> inputs
-        # in this case inputs is a string
-        new_inputs = [ "-O0", "-Xclang", "-disable-O0-optnone", args, "-S", "-emit-llvm", "-o", "-"]
+        self.executable = Alias.clang
+        self.params = [
+            "-O0",
+            "-Xclang",
+            "-disable-O0-optnone",
+            "-S",
+            "-emit-llvm",
+            filename + ".c",
+            "-o",
+            filename + ".ll"
+        ]
 
-        return super(CToLLStage, self).do(new_inputs)
-
-    def processInner(self, std):
-        # return the std output LLVM IR
-        return std
 
 class LLToMem2RegLL(ExternalStage):
-
-    def __init__(self):
-        self.path_to_executable = Alias.opt
+    def __init__(self, filename):
+        super().__init__()
         self.name = "LLVM IR to -mem2reg optimized LLVM IR"
+        self.executable = Alias.opt
+        self.params = [
+            "-mem2reg",
+            filename + ".ll",
+            "-S",
+            "-o",
+            filename + ".mem2reg.ll"
+        ]
 
-    def do(self, args=[], std = None): # f -> inputs
-        # opt -mem2reg ${name}.ll -S -o ${name}.ll
+    def check(self):
+        # Saving the ll file
+        # original_llvm = ll.decode("utf-8")
+        # sanitize ?
+        return True
 
-        new_inputs = [ "-mem2reg", "-", "-S", "-o", "-"]
 
-        return super(LLToMem2RegLL, self).do(new_inputs, std)
-
-    def processInner(self, std):
-        # return the std output optimized LLVM IR
-        return std
-
-class LLToBC(ExternalStage):
-
-    def __init__(self):
-        self.path_to_executable = Alias.llvm_as
+class Mem2RegLLToBC(ExternalStage):
+    def __init__(self, filename):
+        super().__init__()
         self.name = "LLVM IR to LLVM bitcode"
-
-    def do(self, args = [], std = None): # f -> inputs
-        # llvm-as ${name}.ll -o ${name}.bc
-
-        new_inputs = [ "-", "-o", "-"]
-
-        return super(LLToBC, self).do(new_inputs, std)
-
-    def processInner(self, std):
-        # return the std output optimized LLVM IR
-        return std
+        self.executable = Alias.llvm_as
+        self.params = [
+            filename + ".mem2reg.ll",
+            "-o",
+            filename + ".bc"
+        ]
 
 
 class BCToSouper(ExternalStage):
-
-    def __init__(self):
-        self.path_to_executable = Alias.souper
+    def __init__(self, filename):
+        super().__init__()
         self.name = "LLVM IR to LLVM bitcode"
-        
-
-    def do(self, args = [], std = None): # f -> inputs
-        #    souper -z3-path=${z3} ${name}.bc > ${name}.candopt
-
-        new_inputs = [ "-z3-path", Alias.z3, "-"]
-
-        return super(BCToSouper, self).do(new_inputs, std)
-
-    def processInner(self, std):
-        # return the std output optimized LLVM IR
-
-        # Process the candidates, raise error if none and interrupt the pipeline
-
-        return std
+        self.executable = Alias.souper
+        self.params = [
+            "-z3-path",
+            Alias.z3,
+            filename + ".bc",
+            ">",
+            filename + ".cand.opt"
+        ]
 
 
-class CandidatesToSouperParts(ExternalStage):
-
-    def __init__(self):
-        self.path_to_executable = Alias.souper_check
+class SouperToCandidates(ExternalStage):
+    def __init__(self, filename):
+        super().__init__()
         self.name = "Souper candidates to LHS and RHS"
-        
+        self.executable = Alias.souper_check
+        self.params = [
+            "-z3-path",
+            Alias.z3,
+            "-print-replacement-split",
+            filename + ".cand.opt",
+            ">",
+            filename + ".opt"
+        ]
 
-    def do(self, args = [], std = None): # f -> inputs
-        #    souper-check -z3-path=${z3} -print-replacement-split ${name}.candopt > ${name}.opt
+    def check(self):
+        # Saving candidate
+        # self.candidates = cand.decode("utf-8")
+        # Report if no candidates
+        return True
 
-        new_inputs = [ "-z3-path", Alias.z3, "-print-replacement-split", "-"]
 
-        return super(CandidatesToSouperParts, self).do(new_inputs, std)
+class CandidatesToLHS(ExternalStage):
+    def __init__(self, filename):
+        super().__init__()
+        self.name = "from Candidates to LHS"
+        self.executable = "cat"
+        self.params = [
+            filename + ".opt",
+            "|",
+            "sed",
+            "'/^result/d'",
+            ">",
+            filename + ".lhs.opt"
+        ]
 
-    def processInner(self, std):
-        # return the std output optimized LLVM IR
-        return std
+    def check(self):
+        # candtosols = CandidatesToSouperParts()
+        # sols = candtosols(std_in=cand)
+        return True
+
+
+class LHSToRHS(ExternalStage):
+    def __init__(self, filename):
+        super().__init__()
+        self.name = "from LHS to RHS"
+        self.executable = Alias.souper_check
+        self.params = [
+            "-z3-path",
+            Alias.z3,
+            "-infer-rhs",
+            "-souper-infer-iN",
+            filename + ".lhs.opt",
+            ">",
+            filename + ".rhs.opt"
+        ]
+
 
 class Pipeline(object):
-    def process(self, file):
-        
-        ctoll = CToLLStage()
-        ll1 = ctoll.do(file)
-
-        lltoll = LLToMem2RegLL()
-        ll2 = lltoll.do(std=ll1)
-
-        # Saving the ll file
-        self.original_llvm = ll2.decode("utf-8")
-        # sanitize ?
-
-        lltobc = LLToBC()
-        bc = lltobc.do(std=ll2)
-
-        bctocand = BCToSouper()
-        cand = bctocand.do(std=bc)
-
-        #Saving candidate
-        self.candidates = cand.decode("utf-8")
-        # Report if no candidates
+    def __call__(self, file):
+        filename = "infer"
+        CToLLStage(filename)()
+        LLToMem2RegLL(filename)()
+        Mem2RegLLToBC(filename)()
+        BCToSouper(filename)()
+        SouperToCandidates(filename)()
+        CandidatesToLHS(filename)()
+        LHSToRHS(filename)()
 
         # map candidates to original code llvm ?
-
-        candtosols = CandidatesToSouperParts()
-        sols = candtosols.do(std=cand)
-
-        # Map solutions to original optimization candidate
-
-        # Generate LLVM IR for solution
-
-        # Generate Overall LLVM IR output
+        # Map solutions to original optimization candidate?
+        # Generate LLVM IR for solution?
+        # Generate Overall LLVM IR output?
 
 
 if __name__ == "__main__":
-    
     pipeline = Pipeline()
-
-    pipeline.process(sys.argv[1])
+    pipeline(sys.argv[1])
