0863f01a5d0bec9fbfd0cc82965332a83c7812ff
Javier Cabrera <javierca@kth.se>
2019-11-11 18:18:16 +0100

    Sanitizing tests

--- utils/pipeline/python/tests/test_parser.py
+++ utils/pipeline/python/tests/test_parser.py
@@ -1,18 +1,6 @@
 from nodes import SolutionNode
 from dependency import DependencyAnalyzer
 
-def test_LLVM_Block_parser():
-
-    text = '''
-; LGTM
-%0:i32 = var
-%1:i32 = urem %0, 4:i32
-pc %1 2:i32
-%2:i32 = add 1:i32, %0
-infer %2 (demandedBits=00000000000000000000000000000011)
-result 4294967295:i32'''
-
-    s = SolutionNode(text, "fake")
 
 def test_DependencyGraphAnalyze():
     parser = DependencyAnalyzer()
--- utils/pipeline/python/tests/test_pipeline.py
+++ utils/pipeline/python/tests/test_pipeline.py
@@ -57,39 +57,32 @@ def test_lltobc():
     # Receive a LLVM IR in the std
     lltobc = stages.LLToBC()
 
-    lltobc(std=b'''; ModuleID = './python/tests/_dummy/dummy.ll'
-                        source_filename = "./python/tests/_dummy/dummy.c"
-                        target datalayout = "e-m:o-i64:64-f80:128-n8:16:32:64-S128"
-                        target triple = "x86_64-apple-macosx10.14.0"
+    lltobc(std=b'''define i32 @g(i32 %a) #0 {
+                entry:
+                %rem = urem i32 %a, 4
+                switch i32 %rem, label %sw.epilog [
+                    i32 0, label %sw.bb
+                    i32 1, label %sw.bb1
+                    i32 2, label %sw.bb3
+                ]
 
-                        ; Function Attrs: noinline nounwind ssp uwtable
-                        define i32 @a(i32 %t) #0 {
-                        entry:
-                        %tobool = icmp ne i32 %t, 0
-                        br i1 %tobool, label %if.then, label %if.end
+                sw.bb:                                            ; preds = %entry
+                %add = add i32 %a, 3
+                br label %sw.epilog
 
-                        if.then:                                          ; preds = %entry
-                        %add = add nsw i32 20, 20
-                        %mul = mul nsw i32 %add, 2
-                        %add1 = add nsw i32 20, %mul
-                        br label %return
+                sw.bb1:                                           ; preds = %entry
+                %add2 = add i32 %a, 2
+                br label %sw.epilog
 
-                        if.end:                                           ; preds = %entry
-                        br label %return
+                sw.bb3:                                           ; preds = %entry
+                %add4 = add i32 %a, 1
+                br label %sw.epilog
 
-                        return:                                           ; preds = %if.end, %if.then
-                        %retval.0 = phi i32 [ %add1, %if.then ], [ 25, %if.end ]
-                        ret i32 %retval.0
-                        }
-
-                        attributes #0 = { noinline nounwind ssp uwtable "correctly-rounded-divide-sqrt-fp-math"="false" "disable-tail-calls"="false" "less-precise-fpmad"="false" "min-legal-vector-width"="0" "no-frame-pointer-elim"="true" "no-frame-pointer-elim-non-leaf" "no-infs-fp-math"="false" "no-jump-tables"="false" "no-nans-fp-math"="false" "no-signed-zeros-fp-math"="false" "no-trapping-math"="false" "stack-protector-buffer-size"="8" "target-cpu"="penryn" "target-features"="+cx16,+fxsr,+mmx,+sahf,+sse,+sse2,+sse3,+sse4.1,+ssse3,+x87" "unsafe-fp-math"="false" "use-soft-float"="false" }
-
-                        !llvm.module.flags = !{!0, !1}
-                        !llvm.ident = !{!2}
-
-                        !0 = !{i32 1, !"wchar_size", i32 4}
-                        !1 = !{i32 7, !"PIC Level", i32 2}
-                        !2 = !{!"clang version 8.0.0 (tags/RELEASE_800/final 375507)"}
+                sw.epilog:                                        ; preds = %sw.bb3, %sw.bb1, %sw.bb, %entry
+                %a.addr.0 = phi i32 [ %a, %entry ], [ %add4, %sw.bb3 ], [ %add2, %sw.bb1 ], [ %add, %sw.bb ]
+                %and = and i32 %a.addr.0, 3
+                ret i32 %and
+                }
 
                     ''') # Send llvm IR to std
 
@@ -138,3 +131,55 @@ def test_souperIRToLLVM():
 ; RHS inferred successfully
 result 4294967295:i32
                         ''')) # Send llvm IR to std
+
+
+
+def test_partial():
+    # Receive a LLVM IR in the std
+    lltobc = stages.LLToBC()
+
+    bc = lltobc(std=b'''; ModuleID = 'python/tests/_paper/f3.ll'
+source_filename = "python/tests/_paper/f3.c"
+target datalayout = "e-m:o-i64:64-f80:128-n8:16:32:64-S128"
+target triple = "x86_64-apple-macosx10.14.0"
+
+; Function Attrs: noinline nounwind ssp uwtable
+define i32 @g(i32 %a) #0 {
+entry:
+  %rem = urem i32 %a, 4
+  switch i32 %rem, label %sw.epilog [
+    i32 0, label %sw.bb
+    i32 1, label %sw.bb1
+    i32 2, label %sw.bb3
+  ]
+
+sw.bb:                                            ; preds = %entry
+  %add = add i32 %a, 3
+  br label %sw.epilog
+
+sw.bb1:                                           ; preds = %entry
+  %add2 = add i32 %a, 2
+  br label %sw.epilog
+
+sw.bb3:                                           ; preds = %entry
+  %add4 = add i32 %a, 1
+  br label %sw.epilog
+
+sw.epilog:                                        ; preds = %sw.bb3, %sw.bb1, %sw.bb, %entry
+  %a.addr.0 = phi i32 [ %a, %entry ], [ %add4, %sw.bb3 ], [ %add2, %sw.bb1 ], [ %add, %sw.bb ]
+  %and = and i32 %a.addr.0, 3
+  ret i32 %and
+}
+
+attributes #0 = { noinline nounwind ssp uwtable "correctly-rounded-divide-sqrt-fp-math"="false" "disable-tail-calls"="false" "less-precise-fpmad"="false" "min-legal-vector-width"="0" "no-frame-pointer-elim"="true" "no-frame-pointer-elim-non-leaf" "no-infs-fp-math"="false" "no-jump-tables"="false" "no-nans-fp-math"="false" "no-signed-zeros-fp-math"="false" "no-trapping-math"="false" "stack-protector-buffer-size"="8" "target-cpu"="penryn" "target-features"="+cx16,+fxsr,+mmx,+sahf,+sse,+sse2,+sse3,+sse4.1,+ssse3,+x87" "unsafe-fp-math"="false" "use-soft-float"="false" }
+
+!llvm.module.flags = !{!0, !1}
+!llvm.ident = !{!2}
+
+!0 = !{i32 1, !"wchar_size", i32 4}
+!1 = !{i32 7, !"PIC Level", i32 2}
+!2 = !{!"clang version 8.0.0 (tags/RELEASE_800/final 375507)"}
+''') # Send llvm IR to std
+
+    bcToCand = stages.BCToSouper()
+    print(bcToCand(std=bc).decode("utf-8"))
