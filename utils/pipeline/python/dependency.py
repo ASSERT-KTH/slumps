b626446a8a9cbdf70b36d9a611374064b8249a58
Javier Cabrera <javierca@kth.se>
2019-11-11 14:51:23 +0100

    Complete pipeline and one by one candidate generation

--- utils/pipeline/python/dependency.py
+++ utils/pipeline/python/dependency.py
@@ -14,16 +14,15 @@ class DependencyAnalyzer(object):
         labels = []
         # sanitize text
 
-        PATTERN = re.compile('(%(?P<reference>\w+)( )*(?P<assign>=?))|(\n(?P<block>\w+):)')
+        PATTERN = re.compile('(%(?P<reference>\w+)( )*(?P<assign>=?))|(\n(?P<block>\w+):)(?P<endl>\n+)')
         ASSIGN = None
 
         for i in PATTERN.finditer(text):
+            IS_BLOCK = False
 
             if i:
                 groupdict = i.groupdict()
 
-                print(groupdict)
-                
                 if "reference" in groupdict.keys() and i.group("reference"): # label variable reference
                     label = "%"+i.group("reference")
 
@@ -33,17 +32,27 @@ class DependencyAnalyzer(object):
                     if label in nodes.keys():
                         nodes[label]["references"].append(nodes[label]["position"])
                         nodes[label]["position"] = i.span()
-                        nodes[label]["isBlock"] = True
                         continue
+                    
+                    IS_BLOCK = True
+                
+                if "assign" in groupdict.keys() and groupdict["assign"]:
+                    ASSIGN = label
+                
                 if label not in nodes.keys():
+                    
                     labels.append(label)
-                    nodes[label] = dict(label=label, position=i.span(), references=[], isBlock=False, depends_on=[])
-
-                    if "assign" in groupdict.keys():
-                        ASSIGN = label
-
+                    nodes[label] = dict(label=label, position=i.span(), references=[], isBlock=IS_BLOCK, depends_on=[])
                 else:
                     nodes[label]["references"].append(i.span())
-                    nodes[ASSIGN]["depends_on"].append([label, i.span()])
+                
+                nodes[label]["isBlock"] = IS_BLOCK
+
+                if ASSIGN != label and ASSIGN is not None and groupdict["endl"] is None:
+                    if not nodes[label]["isBlock"]: # Not a dependency, jump instead
+                        nodes[ASSIGN]["depends_on"].append([label, i.span()])
+
+
+
                     
         return nodes, labels
