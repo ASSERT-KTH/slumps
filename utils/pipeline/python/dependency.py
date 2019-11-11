267274b1470393acea6db5e0b18d9003b70f3282
Javier Cabrera <javierca@kth.se>
2019-11-11 14:22:52 +0100

    WIP

--- utils/pipeline/python/dependency.py
+++ utils/pipeline/python/dependency.py
@@ -14,26 +14,36 @@ class DependencyAnalyzer(object):
         labels = []
         # sanitize text
 
-        PATTERN = re.compile('(%(\w+))|(\n(\w+):)')
+        PATTERN = re.compile('(%(?P<reference>\w+)( )*(?P<assign>=?))|(\n(?P<block>\w+):)')
+        ASSIGN = None
 
         for i in PATTERN.finditer(text):
 
             if i:
-                if i.group(1): # label variable reference
-                    label = "%"+i.group(2)
-                if i.group(3): # block label reference
-                    label = "%"+i.group(4)
+                groupdict = i.groupdict()
+
+                print(groupdict)
+                
+                if "reference" in groupdict.keys() and i.group("reference"): # label variable reference
+                    label = "%"+i.group("reference")
+
+                if "block" in groupdict.keys() and i.group("block"): # block label reference
+                    label = "%"+i.group("block")
 
                     if label in nodes.keys():
-                        nodes[label]["dependeers"].append(nodes[label]["position"])
+                        nodes[label]["references"].append(nodes[label]["position"])
                         nodes[label]["position"] = i.span()
+                        nodes[label]["isBlock"] = True
                         continue
                 if label not in nodes.keys():
                     labels.append(label)
-                    nodes[label] = dict(label=label, position=i.span(), dependeers=[])
+                    nodes[label] = dict(label=label, position=i.span(), references=[], isBlock=False, depends_on=[])
+
+                    if "assign" in groupdict.keys():
+                        ASSIGN = label
+
                 else:
-                    nodes[label]["dependeers"].append(i.span())
-
-        
-
+                    nodes[label]["references"].append(i.span())
+                    nodes[ASSIGN]["depends_on"].append([label, i.span()])
+                    
         return nodes, labels
