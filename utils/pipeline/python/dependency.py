ded03e0d1f8fb89a10a3d596546669fd10ced507
Javier Cabrera <javierca@kth.se>
2019-11-11 13:03:25 +0100

    WIP

--- utils/pipeline/python/dependency.py
+++ utils/pipeline/python/dependency.py
@@ -9,13 +9,14 @@ class DependencyAnalyzer(object):
     def parse(self, text):
 
         self.nodes = { }
+        self.labels = []
         # sanitize text
 
         label = ''
         comment = False
         new_label = False
         right_part = False
-        breaks = [',', ' ', ':', '\t', '\n', '\r', ")"]
+        breaks = [',', ' ', '\t', '\n', '\r', ")", "}", ";"]
         line = 0
 
         for index, c in enumerate(text):
@@ -29,19 +30,38 @@ class DependencyAnalyzer(object):
                 new_label = True
             if c == '\n':
                 right_part = False
+                
             if c in breaks and new_label:
                 if label.rstrip().lstrip():
                     if label not in self.nodes.keys():
                         self.nodes[label] = { "appear_in": index, "dependeers": [] }
+                        self.labels.append(label)
                     else:
                         self.nodes[label]["dependeers"].append(index)
             
                 label = ''
                 new_label = False
+
+                if c == '\n':
+                    new_label = True
+            if c == ':':
+                # bblock label
+                print(label)
+                if label.rstrip().lstrip():
+                    label = "%"+label
+                    if label not in self.nodes.keys():
+                        self.nodes[label] = { "appear_in": index, "dependeers": [] }
+                        self.labels.append(label)
+                    else:
+                        self.nodes[label]["dependeers"].append(self.nodes[label]["appear_in"])
+                        self.nodes[label]["appear_in"] = index
+            
+                label = ''
+                new_label = False
             if c == '=': # assigment
                 right_part = True
-                
-            if new_label:
+
+            if new_label and c not in breaks:
                 label += c
 
         for  label in self.nodes.keys():
