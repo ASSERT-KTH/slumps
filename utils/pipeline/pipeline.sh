d7d0284e20efeb9fdcc24d49a27352c087bf7a5e
Javier Cabrera <javierca@kth.se>
2019-12-10 15:18:29 +0100

    WIP

--- utils/pipeline/pipeline.sh
+++ utils/pipeline/pipeline.sh
@@ -24,7 +24,8 @@ wasm2wat -o $name.wat $name.wasm
 printf "${RED}Souperifying...${NC}\n"
 
 # "-souper-enumerative-synthesis", "-souper-enumerative-synthesis-num-instructions=%s"%(self.MAX_INST,) souper-infer-inst
-opt -load ${libsouperPass}  -souper -souper-enumerative-synthesis -souper-enumerative-synthesis-num-instructions=5 -souper-enumerative-synthesis-skip-solver  -z3-path=${z3} -souper-debug-level=3 -o $name.opt.bc $name.bc
+opt -load ${libsouperPass}  -souper -souper-enumerative-synthesis -souper-enumerative-synthesis-num-instructions=3 -souper-enumerative-synthesis-skip-solver  -z3-path=${z3} -souper-debug-level=3 -o $name.opt.bc $name.bc
+
 printf "${RED}Souper pass finished${NC}\n"
 
 $WASM_LD --no-entry --export-all -O0 --allow-undefined -o $name.opt.wasm $name.opt.bc
