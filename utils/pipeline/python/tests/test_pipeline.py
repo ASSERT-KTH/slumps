05070693c5f14bf1c80c74ea379a41ed8dcee8a8
Javier Cabrera <javierca@kth.se>
2020-01-14 13:06:18 +0100

    Testing stages individually

--- utils/pipeline/python/tests/test_pipeline.py
+++ utils/pipeline/python/tests/test_pipeline.py
@@ -23,14 +23,32 @@ def test_lltobc():
 
     open("%s/results/babbage_problem.bc"%(BASEDIR,), 'wb').write(result)
 
-def test_bctoSouper():
+def test_bctoSouperCandidates():
     # Receive a LLVM IR in the std
     bctoSouper = stages.BCListCandidates()
     content = open("%s/results/babbage_problem.bc"%(BASEDIR,), 'rb').read()
 
-    lhs = bctoSouper(std=content) # Send llvm IR to std
+    lhs = bctoSouper( std=content) # Send llvm IR to std
 
     open("%s/results/babbage_problem.lhs"%(BASEDIR,), 'wb').write(lhs)
+
+
+def test_bctoWasm():
+    # Receive a LLVM IR in the std
+    bt2wasm = stages.ObjtoWASM()
+
+    bt2wasm( args=["%s/results/babbage_problem.bc"%(BASEDIR,),
+                      "%s/results/babbage_problem.wasm"%(BASEDIR,)]) # Send llvm IR to std
+
+
+
+def test_WASM2WAT():
+    # Receive a LLVM IR in the std
+    wasm2wat = stages.WASM2WAT()
+
+    wasm2wat( args=["%s/results/babbage_problem.wasm"%(BASEDIR,),
+                      "%s/results/babbage_problem.wat"%(BASEDIR,)]) # Send llvm IR to std
+
 
 
 def test_partial():
