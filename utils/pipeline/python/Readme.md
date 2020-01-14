a803ab32c4fbbf1104862a3f488e88dc8dc225ed
Javier Cabrera <javierca@kth.se>
2020-01-14 17:49:40 +0100

    Changing souper

---
+++ utils/pipeline/python/Readme.md
@@ -0,0 +1,10 @@
+## Souper new features
+
+- **souper-subset**: Specify the candidates to be applied, for example
+```-souper-subset=1,2,3,4``` or ```-souper-subset=0,3```
+
+
+- **souper-valid-count**: Search for optimizations without replacing
+
+- **souper-redis-host**: Host for redis
+- **souper-redis-pass**: Password for redis connection
--- utils/pipeline/python/stages.py
+++ utils/pipeline/python/stages.py
@@ -3,6 +3,7 @@
 from subprocess import Popen, PIPE
 from utils import bcolors, DEBUG_FILE, Alias, config
 from logger import LOGGER
+import re
 
 import sys, os
 
@@ -14,11 +15,12 @@ class ExternalStage(object):
         self.path_to_executable = "unknown"
         self.debug = True
 
-    def processInner(self, std):
+    def processInner(self, std, err):
         return b"Ops...Not implemented"
 
     def __call__(self, args=[], stdin=None):  # stdin byte stream
 
+        args = list(filter(lambda x: x != "", args))
         p = Popen([self.path_to_executable] + args, stdout=PIPE, stderr=PIPE, stdin=PIPE)
 
         if stdin is not None:
@@ -26,7 +28,7 @@ class ExternalStage(object):
 
         if self.debug:
             LOGGER.success("Stage -> %s" % (self.name,))
-
+            LOGGER.info(" ".join([self.path_to_executable] + args))
         std, err = p.communicate()
 
         rc = p.returncode
@@ -36,10 +38,11 @@ class ExternalStage(object):
             raise Exception("Error on stage: %s" % (self.name,))
 
         # Specific implementation process over the std out
-        res = self.processInner(std)
+        res = self.processInner(std, err)
 
         if self.debug:
             LOGGER.debug("============================= Stage -> %s\n\n" % (self.name,), res)
+            LOGGER.debug("stderr \n\n %s \n\n" % (err.decode("utf-8"),))
 
         # print("\t%s%s%s'"%(bcolors.WARNING, res, bcolors.ENDC))
 
@@ -58,30 +61,8 @@ class CToLLStage(ExternalStage):
         new_inputs = (config["clang"]["command"] % (args,)).split(" ")
         return super(CToLLStage, self).__call__(new_inputs)
 
-    def processInner(self, std):
+    def processInner(self, std, err):
         return std
-
-
-'''
-class LLToMem2RegLL(ExternalStage):
-
-    def __init__(self):
-        self.path_to_executable = Alias.opt
-        self.name = "LLVM IR to -mem2reg optimized LLVM IR"
-        self.debug = True
-
-    def __call__(self, args=[], std=None):  # f -> inputs
-        # opt -mem2reg ${name}.ll -S -o ${name}.ll
-
-        new_inputs = ["-mem2reg", "-", "-S", "-o", "-"]
-
-        return super(LLToMem2RegLL, self).__call__(new_inputs, std)
-
-    def processInner(self, std):
-        # return the std output optimized LLVM IR
-        return std
-'''
-
 
 class LLToBC(ExternalStage):
 
@@ -95,24 +76,28 @@ class LLToBC(ExternalStage):
         new_inputs = (config["llvm-as"]["command"] % "- -o -").split(" ")
         return super(LLToBC, self).__call__(new_inputs, std)
 
-    def processInner(self, std):
+    def processInner(self, std, err):
         return std
 
 
-class BCListCandidates(ExternalStage):
+class BCCountCandidates(ExternalStage):
 
     def __init__(self):
-        self.path_to_executable = Alias.souper
+        self.path_to_executable = Alias.opt
         self.name = "LLVM BC to Souper IR candidates"
         self.debug = True
 
     def __call__(self, args=[], std=None):  # f -> inputs
+        extra_commands = "%s -o %s" % (args[0], args[0])
 
-        new_inputs = config["souper"]["list-candidates"].split(" ")
-        return super(BCListCandidates, self).__call__(new_inputs, std)
+        new_inputs = (config["souper"]["list-candidates"] % extra_commands).split(" ")
+        return super(BCCountCandidates, self).__call__(new_inputs, std)
 
-    def processInner(self, std):
-        return std
+    def processInner(self, std, err):
+
+        meta = re.compile(r'\[(\d+)/(\d+)\]')
+        match = meta.match(err.decode("utf-8"))
+        return [int(match.group(1)), int(match.group(2))]
 
 
 class BCToSouper(ExternalStage):
@@ -121,14 +106,15 @@ class BCToSouper(ExternalStage):
         self.path_to_executable = Alias.opt
         self.name = "LLVM BC supertoptimization pass"
         self.debug = True
-        self.candidate = candidates
+        self.candidates = candidates
 
     def __call__(self, args=[], std=None):  # f -> inputs
 
-        new_inputs = (config["souper"]["super-opt-pass"] %"%s -o %s"%(args[0], args[1])).split(" ")
+        extra_commands = "-souper-subset=%s %s -o %s" % (",".join(map(lambda x: x.__str__(), self.candidates)), args[0], args[1])
+        new_inputs = (config["souper"]["super-opt-pass"] % extra_commands).split(" ")
         return super(BCToSouper, self).__call__(new_inputs, std)
 
-    def processInner(self, std):
+    def processInner(self, std, err):
         return std
 
 
@@ -144,7 +130,7 @@ class ObjtoWASM(ExternalStage):
         new_inputs = (config["wasm-ld"]["command"] % ("%s %s" % (args[1], args[0]),)).split(" ")
         return super(ObjtoWASM, self).__call__(new_inputs, std)
 
-    def processInner(self, std):
+    def processInner(self, std, err):
         # return the std output optimized LLVM IR
         return std
 
@@ -161,6 +147,6 @@ class WASM2WAT(ExternalStage):
         new_inputs = ['-o', args[1], args[0]]
         return super(WASM2WAT, self).__call__(new_inputs, std)
 
-    def processInner(self, std):
+    def processInner(self, std, err):
         # return the std output optimized LLVM IR
         return std
