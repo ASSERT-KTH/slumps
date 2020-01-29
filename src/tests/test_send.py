6d73abee121e4403f7771a75a8886f6d69158812
Javier Cabrera <javierca@kth.se>
2020-01-29 17:37:37 +0100

    Create issue in Github action

--- src/tests/test_send.py
+++ src/tests/test_send.py
@@ -7,3 +7,15 @@ BASEDIR = os.path.dirname(__file__)
 
 def test_email():
     utils.sendReportEmail("Test report", "Report attached", ["%s/test_re.c"%BASEDIR])
+
+def test_issue():
+    title = 'Pretty title'
+    body = 'Beautiful body'
+    assignee = 'Jacarte'
+    milestone = 1
+    closed = False
+    labels = [
+        "bug"
+    ]
+
+    utils.make_github_issue(title, body, assignee, milestone, closed, labels)
