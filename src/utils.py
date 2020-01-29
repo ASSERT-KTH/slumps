395aee8de1a7e4c6a545b1c3f71175e8e04b777b
Javier Cabrera <javierca@kth.se>
2020-01-29 17:37:37 +0100

    Create issue in Github action

--- src/utils.py
+++ src/utils.py
@@ -13,7 +13,8 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email import encoders
-
+import json
+import requests
 
 config = configparser.ConfigParser(interpolation=configparser.ExtendedInterpolation())
 config.read("settings/config.ini")
@@ -140,6 +141,45 @@ globalCounter.counter = 0
 
 private = configparser.ConfigParser(interpolation=configparser.ExtendedInterpolation())
 private.read("settings/private.ini")
+
+
+
+def make_github_issue(title, body=None, assignee=None, milestone=None,closed=None, labels=None):
+
+    USERNAME = os.environ.get('GITHUB_USER', private["github"]["user"])
+    TOKEN = os.environ.get('GITHUB_TOKEN', private["github"]["token"])
+
+    # The repository to add this issue to
+    REPO_OWNER = USERNAME
+    REPO_NAME = os.environ.get('REPO_NAME', private["github"]["repo_name"])
+
+    # Create an issue on github.com using the given parameters
+    # Url to create issues via POST
+
+    url = 'https://api.github.com/repos/%s/%s/import/issues' % (REPO_OWNER, REPO_NAME)
+
+    # Headers
+    headers = {
+        "Authorization": "token %s" % TOKEN,
+        "Accept": "application/vnd.github.golden-comet-preview+json"
+    }
+
+    # Create our issue
+    data = {'issue': {'title': title,
+                      'body': body,
+                      'milestone': milestone,
+                      'labels': labels}}
+
+    payload = json.dumps(data)
+
+    # Add the issue to our repository
+    response = requests.request("POST", url, data=payload, headers=headers)
+    if response.status_code == 202:
+        print('Successfully created Issue "%s"' % title)
+    else:
+        print('Could not create Issue "%s"' % title)
+        print('Response:', response.content)
+
 
 
 def sendReportEmail(subject, content, attachments = []):
