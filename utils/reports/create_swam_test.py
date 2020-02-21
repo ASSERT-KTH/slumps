b268152caf6d66cc452d95a3639d900fe3c452bd
Javier Cabrera <javierca@kth.se>
2020-02-21 17:34:04 +0100

    Reaftoring benchmark

--- utils/reports/create_swam_test.py
+++ utils/reports/create_swam_test.py
@@ -1,4 +1,7 @@
 import os
+import shutil
 
-for f in os.listdir("/Users/javierca/Documents/Develop/swam/slumps/test/resources/slumps"):
-    print('test("slumps/test/resources/slumps/%s") {run("main")}'%f)
+for f in os.listdir("../../benchmark_programs/variants"):
+    if f != ".DS_Store":
+        shutil.copy(os.path.abspath("../../benchmark_programs/variants/%s/%s.wasm"%(f, f)), "/Users/javierca/Documents/Develop/swam/slumps/test/resources/slumps/%s.wasm"%f )
+        print('test("slumps/test/resources/slumps/%s.wasm") {run("main")}'%f)
