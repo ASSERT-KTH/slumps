3c54d26f971813850f0cacf7c6e47903c0a68791
Javier Cabrera <javierca@kth.se>
2020-03-29 13:25:18 +0200

    Changing executable scripts

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-python3 pipeline.py $FILE
+docker run -it --rm -v $(pwd):/inputs -v $(pwd)/crow_out:/slumps/crow/out -v $pw/crow:/slumps/crow -v $(pwd)/logs:/slumps/crow/logs --entrypoint /bin/bash jacarte/slumps:crow ./start_docker_no_wget.sh /inputs/$1
--- slumps.sh
+++ slumps.sh
@@ -1,25 +1,9 @@
-
-source ~/.bashrc
-
 PS1="(SLUMPS) "
 
-pwd=$(PWD)
+pw=$(PWD)
 shopt -s expand_aliases
 
-export PATH=$pwd/souper/build:$PATH
-
-alias clang='${pwd}/souper/third_party/llvm/Release/bin/clang'
-alias llc='${pwd}/souper/third_party/llvm/Release/bin/llc'
-alias llvm-as='${pwd}/souper/third_party/llvm/Release/bin/llvm-as'
-alias llvm-opt='${pwd}/souper/third_party/llvm/Release/bin/opt'
-alias souper='${pwd}/souper/build/souper'
-alias souper-check='${pwd}/souper/build/souper-check'
-alias wasm2c='${pwd}/wabt/bin/wasm2c'
-alias wasm2wat='${pwd}/wabt/bin/wasm2wat'
-alias wasm-ld='/usr/bin/wasm-ld-8'
-
-# vars
-z3=${pwd}'/souper/third_party/z3/build/z3'
-libsouperPass=${pwd}'/souper/build/libsouperPass.dylib'
+export pw=$pw
 
 # 
+alias crow=$pw/run.sh
