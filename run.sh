3a1f3acbbce682342ba69517225f9a391be40127
Javier Cabrera <javierca@kth.se>
2020-05-28 20:38:18 +0200

    WIP

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 SOURCE=$BASH_SOURCE
 
-docker run -it --rm -v $(pwd):/inputs -v $(pwd)/crow_out:/slumps/crow/out -v $BASH_SOURCE/crow:/slumps/crow -v $(pwd)/logs:/slumps/crow/logs --entrypoint /bin/bash jacarte/slumps:crow1.0 ./start_docker_no_wget.sh /inputs/$1
+docker run -it --rm -v $(pwd):/inputs -v $(pwd)/crow_out:/slumps/crow/out -v $BASH_SOURCE/crow:/slumps/crow -v $(pwd)/logs:/slumps/crow/logs --entrypoint /bin/bash jacarte/slumps:crow ./start_docker_no_wget.sh /inputs/$1 $@
