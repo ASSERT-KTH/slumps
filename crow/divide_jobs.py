b3e97ea60aaacfc7b35eb8888af65c84e9a2f605
Vincent Lohse <vincent.lohse@live.de>
2020-06-10 17:46:03 +0200

    Factored out function in divide_jobs.py

--- crow/divide_jobs.py
+++ crow/divide_jobs.py
@@ -3,39 +3,25 @@ import os
 import sys
 import shutil
 
-if __name__ == '__main__':
-    folder = sys.argv[1]  # folder containing all programs
-    items_per_job = int(sys.argv[2])  # maximum number of programs per job
-    out = sys.argv[3]  # out folder
-    timeout = int(sys.argv[4])  # timeout in seconds per program
+def divide_jobs(programs_dir, max_programs_per_job, out_dir, timeout, extra=""):
 
-    extra = ""
+    if os.path.exists(out_dir):
+        raise Exception(f"Remove output directory first before executing: {out_dir}")
+        shutil.rmtree(out_dir)  # Not running this on my machine
 
-    if len(sys.argv) > 5:
-        extra = " ".join(sys.argv[5:])
+    os.mkdir(out_dir)
 
     jobs = []
-
     currentJob = []
-    for i, file in enumerate(os.listdir(folder)):
-
-        if i % items_per_job == 0 and len(currentJob) > 0:
+    for i, file in enumerate(os.listdir(programs_dir)):
+        if i % max_programs_per_job == 0 and len(currentJob) > 0:
             jobs.append(currentJob)
             currentJob = []
-
         currentJob.append(file)
-
     jobs.append(currentJob)
 
-    # Create a folder for each job
-
-    if os.path.exists(out):
-        shutil.rmtree(out)
-
-    os.mkdir(out)
-
-    shStrip = open("%s/command.sh" % out, 'w')    
-    mpiScript = open("%s/deploy_argo.yml" % out, 'w')
+    shStrip = open(f"{out_dir}/command.sh", 'w')    
+    mpiScript = open(f"{out_dir}/deploy_argo.yml", 'w')
 
     mpiScript.write(
 """
@@ -57,20 +43,18 @@ apiVersion: argoproj.io/v1alpha1
           withItems:
 """)
 
-    for i, j in enumerate(jobs):
-        jobName = "%s/job%s" % (out, i)
-        jobFolder = "job%s"%i
-        if os.path.exists(jobName):
-            shutil.rmtree(jobName)
+    for counter, job in enumerate(jobs):
+        jobName = f"{out_dir}/job{counter}"
+        jobFolder = f"job{counter}"
 
         os.mkdir(jobName)
 
-        for file in j:
-            shutil.copy("%s/%s" % (folder, file), "%s/job%s/%s" % (out, i, file))
+        for file in job:
+            shutil.copy(f"{programs_dir}/{file}", f"{out_dir}/job{counter}/{file}")
 
         # Generate the script
-        shStrip.write("docker run --name {job} -d -e TIMEOUT={timeout} {extra} -v $(pwd)/{job}:/input -v $(pwd)/{job}/out:/slumps/crow/out -v "
-                      "$(pwd)/{job}/logs/:/slumps/crow/logs jacarte/slumps:app \n".format(timeout=timeout, job=jobFolder, extra=extra))
+        shStrip.write(f"docker run --name {jobFolder} -d -e TIMEOUT={timeout} {extra} -v $(pwd)/{jobFolder}:/input -v $(pwd)/{jobFolder}/out:/slumps/crow/out -v "
+                      "$(pwd)/{jobFolder}/logs/:/slumps/crow/logs jacarte/slumps:app \n")
         mpiScript.write(
 """          - { job_folder: %s }\n"""%jobFolder)
 
@@ -88,3 +72,16 @@ apiVersion: argoproj.io/v1alpha1
 
     shStrip.close()
     mpiScript.close()
+
+
+if __name__ == '__main__':
+    programs_dir = sys.argv[1]  # folder containing all programs
+    max_programs_per_job = int(sys.argv[2])
+    out_dir = sys.argv[3]
+    timeout_per_program = int(sys.argv[4])
+
+    extra = ""
+    if len(sys.argv) > 5:
+        extra = " ".join(sys.argv[5:])
+
+    divide_jobs(programs_dir, max_programs_per_job, out_dir, timeout_per_program, extra)
