3cd021f7bd6c7fddd7d6490d9ba2a97d30b4e1bf
Vincent Lohse <vincent.lohse@live.de>
2020-07-06 18:49:36 +0200

    Reading input from file (bytecode) in socket_client

---
+++ run_test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# For testing the integration of prepare_wasm_input.cpp and socket_client.cpp.
+
+# Configured for fibo.wat.
+
+# Run server in Swam first:
+# >> mill -i cli.run run_server --wat --argType Int64 --main naive --out ./ /Users/vincent/not_in_cloud/Codes/KTH/swam/examples/docs/fibo.wat
+
+export WASM_ARG_TYPES_LIST=Int64;
+export WASM_ARG_LIST=8;
+
+g++ -o prepare_wasm_input.out prepare_wasm_input.cpp
+./prepare_wasm_input.out
+
+g++ -o socket_client.out socket_client.cpp
+./socket_client.out
--- socket_client.cpp
+++ socket_client.cpp
@@ -1,4 +1,4 @@
-#include <socket_client.h>
+#include "socket_client.h"
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,9 +9,12 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
+#include <fstream>
 
 #define SOCKET_PORT 9999
 #define SOCKET_HOST "localhost"
+
+// Most code based on: https://www.bogotobogo.com/cplusplus/sockets_server_client.php
 
 void error(const char *msg)
 {
@@ -54,7 +57,7 @@ int connectToServer()
     return sockfd;
 }
 
-// The input file is supposed to have the correct number of 
+// The input file is supposed to have the correct number of
 // bytes. Only call this in the beginning.
 int getRequiredBytes(std::string pathToInput)
 {
@@ -66,9 +69,10 @@ int getRequiredBytes(std::string pathToInput)
 };
 
 // This function aims to write a basic integer to a buffer.
-void fillDummyBuffer(char* buffer) {
+void fillDummyBuffer(char *buffer)
+{
     int64_t fiboNumber = 20;
-    int8_t newLine = 10;  // New line
+    int8_t newLine = 10; // New line
 
     for (int i = 0; i < (sizeof(buffer) - 1); i++)
     {
@@ -83,7 +87,33 @@ void fillDummyBuffer(char* buffer) {
     }
 };
 
-void clientWrite(int sockfd, char* sendBuffer) {
+void readFileToBuffer(char *buffer, std::string pathToInput)
+{
+    std::ifstream rf(pathToInput, std::ios::out | std::ios::binary);
+    if (!rf)
+    {
+        printf("Cannot open prepared_input.dat to read it!\n");
+        exit(1);
+    }
+    rf.read(buffer, sizeof(buffer));
+    rf.close();
+    if (rf.bad())
+    {
+        // Because has not reached end of file.
+        printf("Error occurred at reading time!\n");
+        exit(1);
+    }
+    int8_t newLine = 10; // New line
+    buffer[(sizeof(buffer) - 1)] = newLine;
+
+    for (int i = 0; i < (sizeof(buffer) - 1); i++)
+    {
+        printf("buffer %i: %i \n", i, buffer[i]);
+    }
+}
+
+void clientWrite(int sockfd, char *sendBuffer)
+{
     int n = write(sockfd, sendBuffer, sizeof(sendBuffer));
     std::cout << "Wrote to server!" << std::endl;
     if (n < 0)
@@ -91,7 +121,8 @@ void clientWrite(int sockfd, char* sendBuffer) {
     bzero(sendBuffer, sizeof(sendBuffer));
 }
 
-void clientRead(int sockfd, char* readBuffer) {
+void clientRead(int sockfd, char *readBuffer)
+{
     int n = read(sockfd, readBuffer, sizeof(readBuffer));
     if (n < 0)
         error("ERROR reading from socket");
@@ -99,28 +130,26 @@ void clientRead(int sockfd, char* readBuffer) {
     bzero(readBuffer, sizeof(readBuffer));
 }
 
-int runClient(char* sendBuffer, char* readBuffer)
+int runClient(char *sendBuffer, char *readBuffer)
 {
     int sockfd = connectToServer();
-
-    fillDummyBuffer(sendBuffer);
-
     clientWrite(sockfd, sendBuffer);
-
     clientRead(sockfd, readBuffer);
-
     close(sockfd);
-
     return 0;
 };
 
 int main(int argc, char *argv[])
 {
-    // TODO: Parse input file from args
+    // TODO: Parse input file from args (./prepared_input.dat)
 
-    int requiredBytes = getRequiredBytes("/path/to/input/file");
+    int requiredBytes = getRequiredBytes("./prepared_input.dat");
     char sendBuffer[requiredBytes];
     char readBuffer[4096];
+
+    readFileToBuffer(sendBuffer, "./prepared_input.dat");
+    // fillDummyBuffer(sendBuffer);
+
     runClient(sendBuffer, readBuffer);
     exit(0);
 };
