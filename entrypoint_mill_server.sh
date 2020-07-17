f14d4e906f4884d67f14986601fbf0752d736c16
Vincent Lohse <vincent.lohse@live.de>
2020-07-17 12:28:33 +0200

    Note on where exactly SWAM is compiled

--- entrypoint_mill_server.sh
+++ entrypoint_mill_server.sh
@@ -18,5 +18,6 @@ if [[ $WASI == "True" ]]; then WASI_ARG="--wasi"; fi
 
 cd $REPO_PATH_DOCKER
 
+# This also compiles the cli package in case source code / dependencies have changed: 
 echo "mill -i cli.run run_server $WAT_ARG $WASI_ARG $ALL_ARG_TYPES --main $TARGET_FUNCTION --out $SWAM_OUTPUT_DOCKER $WASM_OR_WAT_FILE"
 mill -i cli.run run_server $WAT_ARG $WASI_ARG $ALL_ARG_TYPES --main $TARGET_FUNCTION --out $SWAM_OUTPUT_DOCKER $WASM_OR_WAT_FILE
