8ee4cd5670225a62c86c980fd1b972ff76c0bb05
Vincent Lohse <vincent.lohse@live.de>
2020-07-28 19:11:26 +0200

    Detached afl & swam entrypoints in main entrypoint

--- wasm-fuzzer/entrypoint.sh
+++ wasm-fuzzer/entrypoint.sh
@@ -3,7 +3,7 @@
 # Initial script: https://docs.docker.com/config/containers/multi-service_container/
 
 # Start the first process
-$DOCKER_SWAM_SRC/entrypoint_mill_server.sh -D
+$DOCKER_SWAM_SRC/entrypoint_mill_server.sh & disown
 status=$?
 if [ $status -ne 0 ]; then
   echo "Failed to start my_first_process: $status"
@@ -11,7 +11,7 @@ if [ $status -ne 0 ]; then
 fi
 
 # Start the second process
-$DOCKER_INTERFACE_SRC/entrypoint_afl.sh -D
+$DOCKER_INTERFACE_SRC/entrypoint_afl.sh & disown
 status=$?
 if [ $status -ne 0 ]; then
   echo "Failed to start my_second_process: $status"
@@ -22,17 +22,19 @@ fi
 # This illustrates part of the heavy lifting you need to do if you want to run
 # more than one service in a container. The container exits with an error
 # if it detects that either of the processes has exited.
-# Otherwise it loops forever, waking up every 60 seconds
+# Otherwise it loops forever, waking up every 30 seconds
 
-while sleep 60; do
-  ps aux |grep my_first_process |grep -q -v grep
-  PROCESS_1_STATUS=$?
-  ps aux |grep my_second_process |grep -q -v grep
-  PROCESS_2_STATUS=$?
+while sleep 30s; do
+  ps aux | grep entrypoint_mill_server.sh |grep -q -v grep
+  MILL_STATUS=$?
+  ps aux | grep entrypoint_afl.sh |grep -q -v grep
+  AFL_STATUS=$?
   # If the greps above find anything, they exit with 0 status
   # If they are not both 0, then something is wrong
-  if [ $PROCESS_1_STATUS -ne 0 -o $PROCESS_2_STATUS -ne 0 ]; then
+  if [ $MILL_STATUS -ne 0 -o $AFL_STATUS -ne 0 ]; then
     echo "One of the processes has already exited."
+    echo "mill status: $MILL_STATUS"
+    echo "afl status: $AFL_STATUS"
     exit 1
   fi
 done
