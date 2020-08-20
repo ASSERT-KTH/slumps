f479eba339d98be8d04b25cc20c498f141f82580
Jacarte <javierca@kth.se>
2020-08-20 10:46:56 +0200

    Improving cli

--- run_docker.sh
+++ run_docker.sh
@@ -9,4 +9,4 @@ PROGRAM_NAME=$(basename $(realpath $PROGRAM))
 
 
 
-docker run -it --rm -v $PROGRAM_DIR:/inputs -v $(pwd)/crow/crow/settings:/slumps/crow/settings  -v $(pwd)/crow_out:/slumps/crow/out  -v $(pwd)/logs:/slumps/crow/logs  --entrypoint /bin/bash jacarte/slumps:crow ./start_poly_bench.sh %DEFAULT.timeout 2000 %DEFAULT.link-wasi False %DEFAULT.outfolder /slumps/crow/out   %DEFAULT.exploration-timeout 1000   /inputs/$PROGRAM_NAME
+docker run -it --rm -v $PROGRAM_DIR:/inputs -v $(pwd)/crow/crow/settings:/slumps/crow/settings  -v $(pwd)/crow_out:/slumps/crow/out  -v $(pwd)/logs:/slumps/crow/logs  --entrypoint /bin/bash jacarte/slumps:crow ./start_poly_bench.sh %DEFAULT.timeout 2000 %DEFAULT.link-wasi False %DEFAULT.outfolder /slumps/crow/out $@ /inputs/$PROGRAM_NAME
