b7c4623bd6cb63f84edb8622d6626b31f339441a
Jacarte <javierca@kth.se>
2020-08-21 16:57:10 +0200

    Adding timeout and killng the server at the end

--- wasm-fuzzer/run_locally.sh
+++ wasm-fuzzer/run_locally.sh
@@ -1,3 +1,6 @@
+# TODO ID the instance and the port for several instances
+
+
 export DOCKER_INTERFACE_SRC=$(pwd)/wafl
 export DOCKER_AFL_INPUT=$(pwd)/wafl/in
 export SWAM_SOCKET_PORT=9999
@@ -8,7 +11,9 @@ export REQUIRED_BYTES=4
 
 export WASM_ARG_TYPES_LIST=Int32
 export WASM_ARG_LIST=15
+
 export DOCKER_LOGS=$(pwd)/wafl/out
+
 export DUMMY_TESTING_AFL=False
 
 
@@ -16,6 +21,8 @@ export AFL="$(pwd)/aflpp/afl-fuzz"
 export LOCAL_AFL="True"
 export WASI_FILTER="True"
 export WASI=True
+export WITH_TIMEOUT="True"
+export TIMEOUT=10 # 10 seconds
 
 mkdir -p $DOCKER_AFL_INPUT
 
@@ -26,14 +33,21 @@ set +x
 if [[ $1 == *.wat ]]; then WAT_ARG="--wat"; fi
 if [[ $WASI == "True" ]]; then WASI_ARG="--wasi"; fi
 if [[ $WASI_FILTER == "True" ]]; then WASI_ARG="$WASI_ARG -r"; fi
+if [[ $WITH_TIMEOUT == "True" ]]; then TIMEOUT="timeout $TIMEOUT"; fi
 
 echo "Running Swam"
-pkill -f out.jar
 
 mkdir -p $DOCKER_AFL_OUTPUT/logs
 
 java -jar fuzzing-server-swam/out/cli/assembly/dest/out.jar run_server $1 -m $2 --argType Int32 $WASI_ARG 1> $DOCKER_AFL_OUTPUT/logs/swam.std.txt 2> $DOCKER_AFL_OUTPUT/logs/swam.err.txt &
 
+swamPid=$!
+
 sleep 2
 echo "Running AFL fuzzing client"
-bash fuzzing-client-afl/entrypoint_afl.sh
+
+$TIMEOUT bash fuzzing-client-afl/entrypoint_afl.sh
+
+echo "Killing swam server pid $swamPid"
+pkill -f $swamPid
+
