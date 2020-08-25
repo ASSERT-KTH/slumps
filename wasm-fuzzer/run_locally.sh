4269c9fec4deb2a1ed1d5d70d4d0b70a908e0cd7
Jacarte <javierca@kth.se>
2020-08-25 09:38:31 +0200

    WIP

--- wasm-fuzzer/run_locally.sh
+++ wasm-fuzzer/run_locally.sh
@@ -1,18 +1,20 @@
-# TODO ID the instance and the port for several instances
+currentID=$$
 
+echo "wafl ID $currentID"
 
 export DOCKER_INTERFACE_SRC=$(pwd)/wafl
-export DOCKER_AFL_INPUT=$(pwd)/wafl/in
+export DOCKER_AFL_INPUT=$DOCKER_INTERFACE_SRC/in-$currentID
+
 export SWAM_SOCKET_PORT=9999
 export SWAM_SOCKET_HOST=0.0.0.0
+export DOCKER_AFL_OUTPUT=out-$currentID
 
-export DOCKER_AFL_OUTPUT=out
 export REQUIRED_BYTES=4
 
-export WASM_ARG_TYPES_LIST=Int32
-export WASM_ARG_LIST=15
+export WASM_ARG_TYPES_LIST=Int32 # Read from signature retriever
+export WASM_ARG_LIST=11 # Read from signature retriever, maybe none
 
-export DOCKER_LOGS=$(pwd)/wafl/out
+export DOCKER_LOGS=$DOCKER_INTERFACE_SRC/out
 
 export DUMMY_TESTING_AFL=False
 
@@ -22,10 +24,11 @@ export LOCAL_AFL="True"
 export WASI_FILTER="True"
 export WASI=True
 export WITH_TIMEOUT="True"
-export TIMEOUT=10 # 10 seconds
+export TIMEOUT=60 # 60 seconds
 
 mkdir -p $DOCKER_AFL_INPUT
 
+## Replace env variables
 set -a
 source .env
 set +x
@@ -33,21 +36,20 @@ set +x
 if [[ $1 == *.wat ]]; then WAT_ARG="--wat"; fi
 if [[ $WASI == "True" ]]; then WASI_ARG="--wasi"; fi
 if [[ $WASI_FILTER == "True" ]]; then WASI_ARG="$WASI_ARG -r"; fi
-if [[ $WITH_TIMEOUT == "True" ]]; then TIMEOUT="timeout $TIMEOUT"; fi
+if [[ $WITH_TIMEOUT == "True" ]]; then TIMEOUT="timeout --foreground $TIMEOUT"; fi
 
 echo "Running Swam"
 
-mkdir -p $DOCKER_AFL_OUTPUT/logs
+mkdir -p $DOCKER_INTERFACE_SRC/logs-$currentID
 
-java -jar fuzzing-server-swam/out/cli/assembly/dest/out.jar run_server $1 -m $2 --argType Int32 $WASI_ARG 1> $DOCKER_AFL_OUTPUT/logs/swam.std.txt 2> $DOCKER_AFL_OUTPUT/logs/swam.err.txt &
+java -jar fuzzing-server-swam/out/cli/assembly/dest/out.jar run_server $1 --main $2 --argType Int32 $WASI_ARG 1> $DOCKER_INTERFACE_SRC/logs-$currentID/swam.std.txt 2> $DOCKER_INTERFACE_SRC/logs-$currentID/swam.err.txt &
 
 swamPid=$!
 
 sleep 2
 echo "Running AFL fuzzing client"
 
-$TIMEOUT bash fuzzing-client-afl/entrypoint_afl.sh
+$TIMEOUT bash fuzzing-client-afl/entrypoint_afl.sh 
 
-echo "Killing swam server pid $swamPid"
 pkill -f $swamPid
-
+echo "Killing swam server pid $swamPid"
