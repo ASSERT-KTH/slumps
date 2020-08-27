87be841e7230c61396b14e15d6fb58201630150b
Vincent Lohse <vincent.lohse@live.de>
2020-08-28 00:45:20 +0200

    Sourcing prepare_env.sh

--- wasm-fuzzer/wafl.sh
+++ wasm-fuzzer/wafl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./prepare_env.sh $@
+source ./prepare_env.sh $@
 
 # TODO: Put this into entrypoint_afl.sh + call function directly in Scala at server startup
 echo "Infering signature for wasm"
