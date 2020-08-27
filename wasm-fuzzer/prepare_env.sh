9f1270fd3946a64a6484deb0c06029c58120ce18
Vincent Lohse <vincent.lohse@live.de>
2020-08-27 23:50:55 +0200

    Using default temp DIR when running locally; Also parsing arguments when in Docker

--- wasm-fuzzer/prepare_env.sh
+++ wasm-fuzzer/prepare_env.sh
@@ -13,23 +13,31 @@ CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 if ! [ -f /.dockerenv ]; then
     echo "Not inside a Docker container";
 
+    TEMP_DIR=$CURRENT_DIR/wafl-temp
+
     export SRC_INTERFACE_DIR=$CURRENT_DIR/fuzzing-client-afl
     export SRC_SWAM_DIR=$CURRENT_DIR/fuzzing-server-swam
 
-    export INPUT_AFL_DIR=$SRC_INTERFACE_DIR/$currentID
-    export OUTPUT_AFL_DIR=$LOCAL_AFL_OUTPUT/$currentID
+    export INPUT_AFL_DIR=$TEMP_DIR/afl-input/$currentID
+    export OUTPUT_AFL_DIR=$TEMP_DIR/afl-output/$currentID
 
-    export LOGS_DIR=$LOCAL_LOGS/$currentID
+    export LOGS_DIR=$TEMP_DIR/logs/$currentID
+
+    export WASM_OR_WAT_FILE=$1
 
     export BIN_AFL="$CURRENT_DIR/aflpp/afl-fuzz"
     export SWAM_CMD="java -jar $SRC_SWAM_DIR/out/cli/assembly/dest/out.jar"
 
-    # mkdir -p $INPUT_AFL_DIR
-    # mkdir -p $OUTPUT_AFL_DIR
-    # mkdir -p $LOGS_DIR
+    mkdir -p $INPUT_AFL_DIR
+    mkdir -p $OUTPUT_AFL_DIR
+    mkdir -p $LOGS_DIR
 else
     echo "Inside a Docker container - env's are pre-defined";
-    SWAM_CMD='mill cli.run'
+
+    # Get filename from $1
+    export WASM_OR_WAT_FILE=$WASM_DIR/$(basename $1)
+    
+    export SWAM_CMD='mill cli.run'
 fi
 
 # TODO: Make this CLI-dependent
@@ -38,7 +46,6 @@ export WASI=True
 
 # TODO: Adjust README to add arguments to Docker run
 # TODO: Check if empty
-export WASM_OR_WAT_FILE=$1
 export TARGET_FUNCTION=$2
 export WASM_ARG_CSV=$3
 
