b7ce78d98ec6d71a8445944dd6842d0ff77e455b
Vincent Lohse <vincent.lohse@live.de>
2020-08-31 19:34:52 +0200

    Created basic script to check whether staging was successful

---
+++ wasm-fuzzer/docker_staging_check.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# TODO: Load wasm file into repository
+
+docker build -t wafl .
+
+yes | docker system prune
+
+set -a
+source ./.env
+set +a
+
+docker run -d --env-file=./.env \
+    -e STAGING=True \
+    --name=staging_wafl \
+    -v maven_data:/root/.cache/coursier/v1/https/repo1.maven.org/maven2 \
+    -v compiled_sources:/home/server/src/out/ \
+    -v ${PWD}/wafl-temp/wat-wasm-files:/home/server/wasm/ \
+    -v ${PWD}/wafl-temp/afl-out:/home/client/out/ \
+    -v ${PWD}/wafl-temp/logs:/home/shared/logs/ \
+    wafl:latest \
+    branches2.wasm a 11
+
+sleep 30s
+
+CONTAINER_STATUS=$(docker container inspect -f '{{.State.Status}}' staging_wafl)
+echo "CONTAINER_STATUS: $CONTAINER_STATUS"
+
+if [ $CONTAINER_STATUS != "running" ]; then
+    echo "staging_wafl container is not running anymore. It must have crashed."
+    exit 1
+else
+    echo "staging_wafl container is still running. Nice!"
+    docker stop staging_wafl
+    exit 0
+fi
