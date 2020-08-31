9eabaa641107605a771f737990f4cc1f16f50d4a
Vincent Lohse <vincent.lohse@live.de>
2020-08-31 19:33:40 +0200

    Added supervisord.staging.conf and stop_supervisord_staging.py for CI

--- wasm-fuzzer/wafl.sh
+++ wasm-fuzzer/wafl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 source ${CURRENT_DIR}/prepare_env.sh $@
 
@@ -10,5 +10,12 @@ echo "$SWAM_CMD infer $WAT_ARG $WASM_OR_WAT_FILE $TARGET_FUNCTION"
 export WASM_ARG_TYPES_CSV=$($SWAM_CMD infer $WAT_ARG $WASM_OR_WAT_FILE $TARGET_FUNCTION) # Read from signature retriever
 pkill -f out.jar
 
+if [[ -z "$STAGING" ]]; then
+    SUPERVISOR_CONF=${CURRENT_DIR}/supervisord.conf
+else
+    SUPERVISOR_CONF=${CURRENT_DIR}/supervisord.staging.conf
+fi
+
 SUPERVISORD_BIN=$(which supervisord)
-exec $SUPERVISORD_BIN -c ${CURRENT_DIR}/supervisord.conf
+
+exec $SUPERVISORD_BIN -c $SUPERVISOR_CONF
