6a2cffde9502c4ba24de6dbc2edfe52578b8af16
Jacarte <javierca@kth.se>
2020-09-09 07:17:56 +0200

    Removing failing CI

--- run_docker.sh
+++ run_docker.sh
@@ -9,8 +9,5 @@ PROGRAM_NAME=$(basename $(realpath $PROGRAM))
 
 
 
-docker run -it --rm -v $PROGRAM_DIR:/inputs -v $(pwd)/crow/crow/settings:/slumps/crow/settings  -v $(pwd)/crow_out:/slumps/crow/out  -v $(pwd)/logs:/slumps/crow/logs  --entrypoint /bin/bash slumps/crow:latest ./start_poly_bench.sh $1 %DEFAULT.timeout 3600 %DEFAULT.exploration-timeout 30 %souper.workers 10 %DEFAULT.link-wasi False %DEFAULT.outfolder /slumps/crow/out $@ /inputs/$PROGRAM_NAME
+docker run -it --rm -v $PROGRAM_DIR:/inputs -v $(pwd)/crow/crow/settings:/slumps/crow/crow/settings -v $(pwd)/crow/crow:/slumps/crow/cro  -v $(pwd)/crow_out:/slumps/crow/out  -v $(pwd)/logs:/slumps/crow/logs  --entrypoint /bin/bash slumps/crow:latest ./start_poly_bench.sh $1 %DEFAULT.timeout 3600 %DEFAULT.exploration-timeout 2000 %souper.workers 30 %DEFAULT.link-wasi False %DEFAULT.outfolder /slumps/crow/out $@ /inputs/$PROGRAM_NAME
 
-
-
-#docker run -it --rm -v $PROGRAM_DIR:/inputs -v $(pwd)/crow/crow/settings:/slumps/crow/settings  -v $(pwd)/crow_out:/slumps/crow/out  -v $(pwd)/logs:/slumps/crow/logs  --entrypoint /bin/bash jacarte/slumps:crow ./start_poly_bench.sh %DEFAULT.timeout 86400 %souper.souper-common --solver-timeout=1600 %DEFAULT.link-wasi False %DEFAULT.outfolder /slumps/crow/out $@ /inputs/$PROGRAM_NAME
