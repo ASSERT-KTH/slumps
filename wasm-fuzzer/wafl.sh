b063724a273eb52dbf765d65ef7c9bd9af689225
Vincent Lohse <vincent.lohse@live.de>
2020-09-12 12:54:06 +0200

    Inferring sig in prepare_env.sh; entrypoint_afl.sh working independently

--- wasm-fuzzer/wafl.sh
+++ wasm-fuzzer/wafl.sh
@@ -6,12 +6,6 @@ CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 source ${CURRENT_DIR}/prepare_env.sh $@
 
-# TODO: Put this into entrypoint_afl.sh + call function directly in Scala at server startup
-echo "Infering signature for wasm"
-echo "$SWAM_CMD infer $WAT_ARG $WASM_OR_WAT_FILE $TARGET_FUNCTION"
-export WASM_ARG_TYPES_CSV=$($SWAM_CMD infer $WAT_ARG $WASM_OR_WAT_FILE $TARGET_FUNCTION) # Read from signature retriever
-pkill -f out.jar
-
 if [[ -z "$STAGING" ]]; then
     SUPERVISOR_CONF=${CURRENT_DIR}/supervisord.conf
 else
