029d9a5b35fc89b4ea9320a884a585081ca65104
Jacarte <javierca@kth.se>
2020-09-15 12:05:15 +0200

    FIX: CI

--- wasm-fuzzer/build.sh
+++ wasm-fuzzer/build.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+err=0
+trap 'err=1' ERR
+
 CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
+echo $CURRENT_DIR
+
 
 echo "Cloning SWAM"
 if [ ! -d fuzzing-server-swam ]; then
-    git clone --single-branch --branch master https://github.com/KTH/swam.git fuzzing-server-swam
+    git clone --single-branch --branch slumps https://github.com/KTH/swam.git fuzzing-server-swam
 fi
 
 echo "Building SWAM"
@@ -12,9 +18,10 @@ cd fuzzing-server-swam
 git pull
 ./millw cli.assembly
 
+
 cd ..
 
-$CURRENT_DIR/fuzzing-client-afl/build_afl.sh
-$CURRENT_DIR/fuzzing-client-afl/build_interface.sh
+bash $CURRENT_DIR/fuzzing-client-afl/build_afl.sh
+bash $CURRENT_DIR/fuzzing-client-afl/build_interface.sh
 
-echo "DONE!"
+test $err = 0
