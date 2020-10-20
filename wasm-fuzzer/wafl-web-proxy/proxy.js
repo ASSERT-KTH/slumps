103d1fdff693a498f2b9daca5b06be45af845da5
Jacarte <javierca@kth.se>
2020-10-20 16:56:49 +0200

    Unifying proxy

--- wasm-fuzzer/wafl-web-proxy/proxy.js
+++ wasm-fuzzer/wafl-web-proxy/proxy.js
@@ -1,6 +1,9 @@
 const AnyProxy = require('anyproxy');
+const fs = require("fs");
 
-console.log(process.env.INSTRUMENTER_ADDRESS)
+const MASKED_URL="https://wafl.live"
+
+console.log(MASKED_URL)
 const options = {
   port: 8080,
   throttle: 10000,
@@ -13,25 +16,67 @@ const options = {
     webPort: 8002
   },
   rule: {
-	beforeSendResponse: async function (requestDetail, responseDetail) {
-		//console.log(responseDetail.response.header)
-		
-		
+	
+	beforeSendRequest: async function(requestDetail) {
+		if(requestDetail.url.indexOf(MASKED_URL) !== -1){
+			const wrapper_script = requestDetail.url.replace(MASKED_URL, "")
+			console.log("Internal processing",wrapper_script)
+			if(wrapper_script.indexOf("/static") !== -1){ // return static content
+				const script_file = wrapper_script.replace("/static", "")
+				console.log("Loading script content",script_file)
+				const content = fs.readFileSync(`./static/${script_file}`);
+				return {
+					response: {
+						statusCode: 200,
+						header: { 'content-type': 'text/javascript' },
+						body: content
+					  }
+				}
+			}
 
-		if("Content-Type" in responseDetail.response.header) // DO NOT change WASM files
-		{
-			//console.log(responseDetail.response.header["Content-Type"])
-			if(responseDetail.response.header["Content-Type"].indexOf("application/octet-stream") !== -1)
+			return {
+				response: {
+					statusCode: 400,
+					header: { 'content-type': 'text/html' },
+					body: "Not found"
+				  }
+			}
+		}
+
+	},
+	beforeSendResponse: async function (requestDetail, responseDetail) {
+
+		if(responseDetail.response.statusCode !== 200)
+			return
+		if(responseDetail.response.header["Content-Type"]){
+			if(responseDetail.response.header["Content-Type"].indexOf("text/html") === -1) // Instrument only html responses
 				return
 		}
+
 		// INJECT WAFL script
 		let data = responseDetail.response.body.toString();
-		const routerJS  = `<script type="text/javascript">window.INSTRUMENTER_HOST='${process.env.INSTRUMENTER_ADDRESS}'</script>\n`
-		const instrumentationJS = `<script src="${process.env.INSTRUMENTER_ADDRESS}/static/${process.env.INSTRUMENTATION_TYPE}"></script>\n`
+		const routerJS  = `<script type="text/javascript">window.INSTRUMENTER_HOST='${MASKED_URL}'</script>\n`
+
+		const content = fs.readFileSync(`./static/${process.env.INSTRUMENTATION_TYPE}`);
+		const dashboardIndex = fs.readFileSync(`./static/index.js`);
+
+		const instrumentationJS = `<script type="text/javascript">${content}</script>\n`
+		const dashBoardJS = `<script type="text/javascript">${dashboardIndex}</script>\n`
 		data = data.replace("<head>", `<head>${routerJS}${instrumentationJS}\n`)
-		console.log(data.indexOf("</head>"))
+
+		if(data.indexOf("</body>") > -1)
+			data = data.replace("</body>", `\n${dashBoardJS}\n</body>\n`)
+		if(data.indexOf("<body>") > -1)
+			data = data.replace("<body>", `<body>\n${dashBoardJS}\n`)
+
+		console.log(data)
+		
+		if(data.indexOf("</head>") > -1)
+			console.log(data.indexOf("</head>"))
+
 		const newResponse = responseDetail.response;
 		newResponse.body = Buffer.from(data, 'utf-8')
+		
 		// SET the security policy of the site
 		newResponse.header = {...newResponse.header, 'content-security-policy':  "script-src * 'unsafe-inline' 'unsafe-eval';		"}
 
@@ -53,3 +98,6 @@ proxyServer.start();
 
 //when finished
 //proxyServer.close();
+
+/*
+ */
