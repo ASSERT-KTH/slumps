fa0aadd891008c497892cc1ea644aa0ad8f1056b
Jacarte <javierca@kth.se>
2020-11-03 22:30:01 +0100

    Injecting code instead of networking

--- wakoko_extension/wakoko/build_extension.sh
+++ wakoko_extension/wakoko/build_extension.sh
@@ -1,4 +1,5 @@
 yarn build
 
-cd build
-zip -r -FS ../wakoko_chrome.zip *
+cp src/content_template.js build/static/js/content.js
+cp src/content.css build/static/css/content.css
+perl -pe 's/WRAPPER/`cat "build\/static\/js\/wrapper.js"`/ge' -i build/static/js/content.js
