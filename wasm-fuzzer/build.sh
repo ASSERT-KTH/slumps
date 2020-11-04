75d68c0e65ad0a7de93acc495922b245f63227eb
Vincent Lohse <vincent.lohse@live.de>
2020-11-04 19:08:17 +0100

    millw only working when in correct directory

--- wasm-fuzzer/build.sh
+++ wasm-fuzzer/build.sh
@@ -4,7 +4,8 @@ CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 source $CURRENT_DIR/logging_lib.sh
 
 log_info "Building SWAM"
-./fuzzing-server-swam/millw cli.assembly
+cd $CURRENT_DIR/fuzzing-server-swam
+./millw cli.assembly
 
 bash $CURRENT_DIR/fuzzing-client-afl/build_interface.sh
 bash $CURRENT_DIR/fuzzing-client-afl/build_afl.sh
