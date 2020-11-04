a2d27ae67bcdcf036591f0fe80c1bbb8ac487844
Vincent Lohse <vincent.lohse@live.de>
2020-11-04 18:43:54 +0100

    Not cloning SWAM in build.sh anymore because submodule again

--- wasm-fuzzer/build.sh
+++ wasm-fuzzer/build.sh
@@ -3,11 +3,6 @@
 CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 source $CURRENT_DIR/logging_lib.sh
 
-log_info "Cloning SWAM"
-if [ ! -d fuzzing-server-swam ]; then
-    git clone --single-branch --branch slumps https://github.com/KTH/swam.git fuzzing-server-swam
-fi
-
 log_info "Building SWAM"
 ./fuzzing-server-swam/millw cli.assembly
 
