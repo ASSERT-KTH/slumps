cd436877942f59327615acf8161c7dc5acdcd70f
Vincent Lohse <vincent.lohse@live.de>
2020-11-04 18:14:35 +0100

    Added proper logging to build processes

--- wasm-fuzzer/build.sh
+++ wasm-fuzzer/build.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
 CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+source $CURRENT_DIR/logging_lib.sh
 
-echo $CURRENT_DIR
-
-echo "Cloning SWAM"
+log_info "Cloning SWAM"
 if [ ! -d fuzzing-server-swam ]; then
     git clone --single-branch --branch slumps https://github.com/KTH/swam.git fuzzing-server-swam
 fi
 
-echo "Building SWAM"
+log_info "Building SWAM"
 ./fuzzing-server-swam/millw cli.assembly
 
-bash $CURRENT_DIR/fuzzing-client-afl/build_afl.sh
 bash $CURRENT_DIR/fuzzing-client-afl/build_interface.sh
+bash $CURRENT_DIR/fuzzing-client-afl/build_afl.sh
 
-echo "Finishing"
+log_info "Finished building WAFL"
