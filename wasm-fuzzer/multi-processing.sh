aa10b22ce1e87e383ed47cfc5c2e71b4bdf1acb7
Vincent Lohse <vincent.lohse@live.de>
2020-11-05 11:47:09 +0100

    Changed PWD to  CURRENT_DIR references in testing scripts

--- wasm-fuzzer/multi-processing.sh
+++ wasm-fuzzer/multi-processing.sh
@@ -21,14 +21,14 @@ set +a
 rm -R $CURRENT_DIR/wafl-temp
 
 log_info "Running #1"
-docker run --env-file=./.env \
+docker run --env-file=${CURRENT_DIR}/.env \
     -e MASTER_AFL_NODE=True \
     -e WAFL_INSTANCE_ID=1 \
     -v maven_data:/root/.cache/coursier/v1/https/repo1.maven.org/maven2 \
     -v compiled_sources:/home/server/src/out/ \
     -v ${LOCAL_WASM_DIR:?err}:/home/server/wasm/ \
-    -v ${PWD}/wafl-temp/afl-out:/home/client/out/ \
-    -v ${PWD}/wafl-temp/logs/1:/home/shared/logs/ \
+    -v ${CURRENT_DIR}/wafl-temp/afl-out:/home/client/out/ \
+    -v ${CURRENT_DIR}/wafl-temp/logs/1:/home/shared/logs/ \
     -d wafl:latest $@
 
 if [ $NUM_INSTANCES -lt 2 ]; then
@@ -39,14 +39,14 @@ for i in $(seq 2 $NUM_INSTANCES); do
     log_info "Waiting for previous mill server to compile..."
     sleep 30s
     log_info "Running #${i}"
-    docker run --env-file=./.env \
+    docker run --env-file=${CURRENT_DIR}/.env \
         -e MASTER_AFL_NODE=False \
         -e WAFL_INSTANCE_ID=${i} \
         -v maven_data:/root/.cache/coursier/v1/https/repo1.maven.org/maven2 \
         -v compiled_sources:/home/server/src/out/ \
         -v ${LOCAL_WASM_DIR:?err}:/home/server/wasm/ \
-        -v ${PWD}/wafl-temp/afl-out:/home/client/out/ \
-        -v ${PWD}/wafl-temp/logs/${i}:/home/shared/logs/ \
+        -v ${CURRENT_DIR}/wafl-temp/afl-out:/home/client/out/ \
+        -v ${CURRENT_DIR}/wafl-temp/logs/${i}:/home/shared/logs/ \
         -d wafl:latest $@
 done
 exit 0
