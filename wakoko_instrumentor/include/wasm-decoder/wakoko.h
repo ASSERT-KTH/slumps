4071c8e1345d158b45ce5359765a1b269bf2a369
Jacarte <javierca@kth.se>
2020-11-27 11:43:09 +0100

    Adding bypassing option

--- wakoko_instrumentor/include/wasm-decoder/wakoko.h
+++ wakoko_instrumentor/include/wasm-decoder/wakoko.h
@@ -5,6 +5,6 @@
 #define NEW_CODE_BUFFER_SIZE (1 << 18)
 #define NEW_ID_BUFFER_SIZE 10
 
-void make_coverage_instrumentation(WASMModule* module, int *global_pad, int* global_count, int threshold, float instrumentation_probability);
+void make_coverage_instrumentation(WASMModule* module, int *global_pad, int* global_count, int threshold, float instrumentation_probability, int* instruction_count);
 
 #endif
