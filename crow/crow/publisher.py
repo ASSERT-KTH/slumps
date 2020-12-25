57cd805e9720caf45448d7cc9a0764d2d6bb7e10
Jacarte <javierca@kth.se>
2020-12-25 19:45:03 +0100

    WIP

--- crow/crow/publisher.py
+++ crow/crow/publisher.py
@@ -1,6 +1,9 @@
 import pika
 from settings import config
 from logger import LOGGER, getlogfilename
+import sys
+import json
+import base64
 
 class Publisher:
 
@@ -12,8 +15,8 @@ class Publisher:
 		# verifies that it is of the correct and expected class.
 		channel.exchange_declare(exchange=config["eb"]["exchange"], exchange_type=config["eb"]["exchange-type"])
 		#Publishes message to the exchange with the given routing key
-		channel.basic_publish(exchange=config["eb"]["exchange"], routing_key=routing_key, body=message)
-		print("[x] Sent message %r for %r" % (message,routing_key))
+		channel.basic_publish(exchange=config["eb"]["exchange"], routing_key=routing_key, body=json.dumps(message))
+		print("[x] Sent message")
 		#LOGGER.info("events","[x] Sent message %r for %r" % (message,routing_key))
 
   # Create new connection
@@ -24,4 +27,11 @@ class Publisher:
 
 if __name__ == "__main__":
 	publisher = Publisher()
-	publisher.publish('nse.nifty', 'New Data')
+	for i in range(1):
+		publisher.publish(config["eb"]["process-id"], dict(
+			name="test.bc",
+			bitcode=base64.b64encode(open(sys.argv[1], 'rb').read()).decode(),
+			set={
+				"%0:i32 = var (knownBits=0xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx) (nonNegative) (range=[0,100))\n%1:i32 = addnsw 1:i32, %0 (hasExternalUses)\n%2:i1 = slt %1, 100:i32\ninfer %2\n": "%3:i1 = and %2, %2\n%4:i1 = and %3, %2\n%5:i1 = and %4, %2\n%6:i1 = and %5, %2\n%7:i1 = and %6, %2\n%8:i1 = and %7, %2\n%9:i1 = and %8, %2\n%10:i1 = and %9, %2\n%11:i1 = and %10, %2\n%12:i1 = and %11, %2\n%13:i1 = and %12, %2\n%14:i1 = and %13, %2\n%15:i1 = and %14, %2\n%16:i1 = and %15, %2\nresult %16\n"
+			}
+		))
