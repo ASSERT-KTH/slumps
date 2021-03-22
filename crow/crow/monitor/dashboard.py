2c1dfcd70dd574eb2a0492f73b9b131535ca1200
Jacarte <javierca@kth.se>
2021-03-22 21:11:33 +0100

    Adding dash board and downloading options

--- crow/crow/monitor/dashboard.py
+++ crow/crow/monitor/dashboard.py
@@ -14,24 +14,31 @@ import traceback
 from crow.settings import config
 from crow.monitor import MONITOR_QUEUE_NAME
 from crow.utils import printProgressBar
+from zipfile import ZipFile
+import os
+import flask
+from shutil import make_archive
+from flask.helpers import send_file
+
 
 PROGRAM_RE = re.compile(r"^(.+?):")
+BASE_DIR = os.path.dirname(__file__)
 
-'''
+if __name__ == "__main__":
+
+    redis_host = sys.argv[1]
+    redis_port = int(sys.argv[2])
+    redis_db = sys.argv[3]
+    redis_pass = sys.argv[4]
+    out_folder = sys.argv[5]
+
+    connection = redis.Redis(host=redis_host,port=redis_port, db=redis_db, password=redis_pass)
+    app = dash.Dash(external_stylesheets=[dbc.themes.LUX])
 
 
-                        
-'''
-
-class Monitor:
-
-    def __init__(self, redis_host, redis_port, redis_db, redis_pass):
-        self.connection = redis.Redis(host=redis_host,port=redis_port, db=redis_db, password=redis_pass)
-        self.redis_db = redis_db
-
-    def get_all_programs(self):
+    def get_all_programs():
         print("getting programs")
-        allkeys = self.connection.keys("*")
+        allkeys = connection.keys("*")
 
         r = { }
 
@@ -44,61 +51,97 @@ class Monitor:
 
         return r
 
-    def get_unique_bitcodes(self, program):
-        bc_keys = self.connection.keys(f"{program}:bc:*")
+    def get_unique_bitcodes(program):
+        bc_keys = connection.keys(f"{program}:bc:*")
         return bc_keys
 
 
-    def get_eq_variants_for_bc(self, bitcode_key):
-        variants = self.connection.get(f"{bitcode_key.decode()}")
+    def get_eq_variants_for_bc(bitcode_key):
+        variants = connection.get(f"{bitcode_key.decode()}")
         return json.loads(variants.decode()) if variants else None
 
 
-    def get_eq_variants_for_wasm(self, wasm_key):
-        variants = self.connection.get(f"{wasm_key.decode()}")
+    def get_eq_variants_for_wasm(wasm_key):
+        variants = connection.get(f"{wasm_key.decode()}")
         return json.loads(variants.decode()) if variants else None
 
-    def get_eq_variants_for_wat(self, wat_key):
-        variants = self.connection.get(f"{wat_key.decode()}")
+    def get_eq_variants_for_wat( wat_key):
+        variants = connection.get(f"{wat_key.decode()}")
         return json.loads(variants.decode()) if variants else None
 
-    def get_unique_wasm(self, program):
-        wasm_keys = self.connection.keys(f"{program}:wasm:*")
+    def get_unique_wasm(program):
+        wasm_keys = connection.keys(f"{program}:wasm:*")
         return wasm_keys
 
-    def get_unique_wat(self, program):
-        wat_keys = self.connection.keys(f"{program}:wat:*")
+    def get_unique_wat(program):
+        wat_keys = connection.keys(f"{program}:wat:*")
         return wat_keys
 
-    def get_total_llvm(self, program):
+    def get_total_llvm(program):
         C = 0
-        for bc in self.get_unique_bitcodes(program):
-            C += len(self.get_eq_variants_for_bc(bc))
+        for bc in get_unique_bitcodes(program):
+            C += len(get_eq_variants_for_bc(bc))
         return C
 
-    def get_total_wasm(self, program):
+    def get_total_wasm(program):
         C = 0
-        for bc in self.get_unique_wasm(program):
-            C += len(self.get_eq_variants_for_wasm(bc))
+        for bc in get_unique_wasm(program):
+            C += len(get_eq_variants_for_wasm(bc))
         return C
 
-    def get_total_wat(self, program):
+    def get_total_wat(program):
         C = 0
-        for bc in self.get_unique_wat(program):
-            C += len(self.get_eq_variants_for_wat(bc))
+        for bc in get_unique_wat(program):
+            C += len(get_eq_variants_for_wat(bc))
         return C
 
-    def deploy_dash(self, debug=True):
+    def make_zip(path, name="out"):
+        print(path, os.listdir(path))
+
+        make_archive(
+            f'{BASE_DIR}/{name}',
+            'zip',  # the archive format - or tar, bztar, gztar
+            root_dir=path,  # root for archive - current working dir if None
+            base_dir=None)  # start archiving from here - cwd if None too
+
+        return f"{name}.zip"
+
+    @app.server.route('/downloadable/<path:program_name>')
+    def serve_static_bitcodes(program_name):
+
+        o = make_zip(f"{out_folder}/{program_name}/bitcodes/variants", "bitcodes")
+
+        return flask.send_from_directory(
+            BASE_DIR, o, as_attachment=True,cache_timeout=0
+        )
+
+
+    @app.server.route('/downloadablew/<path:program_name>')
+    def serve_static_wasms(program_name):
+
+        o = make_zip(f"{out_folder}/{program_name}/wasm", "wasms")
+
+        return flask.send_from_directory(
+            BASE_DIR, o, as_attachment=True, cache_timeout=0
+        )
+
+
+    @app.callback(dash.dependencies.Output('page-content', 'children'),
+                  [dash.dependencies.Input('url', 'pathname'),
+                    dash.dependencies.Input('update-button', 'n_clicks')],
+                  )
+    def display_page(pathname, n_clicks):
 
         def plot_general_stats():
 
-            programs = list(self.get_all_programs().keys())
+            programs = list(get_all_programs().keys())
 
             x = programs
-            y = [ len(self.get_unique_bitcodes(p)) for p in programs]
-            t = [ self.get_total_llvm(p) for p in programs]
+            y = [ len(get_unique_bitcodes(p)) for p in programs]
+            t = [ get_total_llvm(p) for p in programs]
 
-            return dcc.Graph(
+            return None
+            '''dcc.Graph(
                             id=f'GeneralGraph',
                             figure={
                                 'data': [
@@ -130,31 +173,26 @@ class Monitor:
                                 'marginBottom': '60px'
                             }
                         )
-
+            '''
 
         def generate_report_for_module(module):
 
-            unique_llvm = len(self.get_unique_bitcodes(module))
-            unique_wasm = len(self.get_unique_wasm(module))
-            unique_wat = len(self.get_unique_wat(module))
+            unique_llvm = len(get_unique_bitcodes(module))
+            unique_wasm = len(get_unique_wasm(module))
+            unique_wat = len(get_unique_wat(module))
 
-            total_llvm = self.get_total_llvm(module)
-            total_wasm = self.get_total_wasm(module)
-            total_wat = self.get_total_wat(module)
+            total_llvm = get_total_llvm(module)
+            total_wasm = get_total_wasm(module)
+            total_wat = get_total_wat(module)
 
-
+            '''
+            
+            '''
             return html.Div(
                 children=html.Div(
                     children=[
                         html.H2(
                             children=[
-                                html.A(
-                                    className="btn btn-primary",
-                                    children="Download original bitcode (TODO)",
-                                    style={
-                                        'marginRight': '50px'
-                                    }
-                                ),
 
                                 html.A(
                                     children=module
@@ -169,11 +207,28 @@ class Monitor:
                                 html.H4(
                                     className="col",
                                     children=f"Unique LLVM {unique_llvm}/{total_llvm} ({unique_llvm/total_llvm*100:.2f}%)"
+                                ),
+                                html.A(
+                                    className="btn btn-primary",
+                                    children="Download bitcodes",
+                                    href=f"/downloadable/{module}",
+                                    style={
+                                        'marginRight': '50px'
+                                    }
                                 )
                                 ,
                                 html.H4(
                                     className="col",
                                     children=f"Unique WASM {unique_wasm}/{total_wasm} ({unique_wasm/total_wasm*100:.2f}%)"
+                                ),
+
+                                html.A(
+                                    className="btn btn-primary",
+                                    children="Download wasms",
+                                    href=f"/downloadablew/{module}",
+                                    style={
+                                        'marginRight': '50px'
+                                    }
                                 )
                             ]
                         ),
@@ -185,72 +240,82 @@ class Monitor:
                     'color': colors['text']
                 }
             )
+        if pathname == '/':
+            programs = get_all_programs()
+            colors = {
+                'background': '#FFFFFF',
+                'text': '#000000'
+            }
+            return html.Div(style={'backgroundColor': colors['background']}, children=[
+                html.Div(
+                    children=[
+                        html.H1(
+                            children='CROW dashboard'
+                        ),
+                        html.H2(
+                            children=f"DB {redis_db}"
+                        ),
+                        html.H2(
+                            children=f"{len(programs)} Modules "
+                        ),
+                        plot_general_stats()
+                    ],
+                    style={
+                        'textAlign': 'center',
+                        'color': colors['text']
+                    }
+                ),
+                html.Div(
+                    className="container",
+                    children=list(map(generate_report_for_module, programs))
+                )
 
-        app = dash.Dash(external_stylesheets=[dbc.themes.LUX])
-        programs = self.get_all_programs()
-        colors = {
-            'background': '#FFFFFF',
-            'text': '#000000'
-        }
-        app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
-            html.Div(
-                children=[
-                    html.H1(
-                        children='CROW dashboard'
-                    ),
-                    html.H2(
-                        children=f"DB {self.redis_db}"
-                    ),
-                    html.H2(
-                        children=f"{len(programs)} Modules "
-                    ),
-                    plot_general_stats()
-                ],
+            ])
+
+    def deploy_dash(debug=True):
+
+
+
+        app.layout = html.Div(children=[
+            dcc.Location(id='url', refresh=False),
+            html.A(
+                className="btn btn-default",
+                id="update-button",
+                children="Reload",
                 style={
-                    'textAlign': 'center',
-                    'color': colors['text']
+                    'marginRight': '50px'
                 }
             ),
-            html.Div(
-                className="container",
-                    children=list(map(generate_report_for_module, programs))
-            )
-
+            html.Div(id='page-content'),
         ])
-
-
+        '''
+        
+    
+        '''
         app.run_server(debug=debug)
 
     def print_general_stats(self):
-        programs = self.get_all_programs()
+        programs = get_all_programs()
 
         for k in programs.keys():
 
             print(k)
-            bc_unique = self.get_unique_bitcodes(k)
+            bc_unique = get_unique_bitcodes(k)
             print(f"\t bitcodes {len(bc_unique)}")
 
 
             for bc in bc_unique:
-                variants = self.get_eq_variants_for_bc(bc)
+                variants = get_eq_variants_for_bc(bc)
                 print(f"\t\teq {len(variants)}")
 
 
-            w_unique = self.get_unique_wasm(k)
+            w_unique = get_unique_wasm(k)
             print(f"\t wasm {len(w_unique)}")
 
-            wt_unique = self.get_unique_wat(k)
+            wt_unique = get_unique_wat(k)
             print(f"\t wat {len(wt_unique)}")
-
-if __name__ == "__main__":
-
-    m = Monitor(
-        sys.argv[1],
-        int(sys.argv[2]),
-        sys.argv[3],
-        sys.argv[4]
-    )
-
-    m.deploy_dash()
+    
     #subscriber = Subscriber(1, MONITOR_QUEUE_NAME, GENERATED_BC_KEY, config["event"].getint("port"), general_log)
     #subscriber.setup()
+
+    deploy_dash()
