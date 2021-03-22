b7d9b82211f8893097e4e13bc775dea98a86dfe5
Jacarte <javierca@kth.se>
2021-03-22 22:24:59 +0100

    Initial dashboard version

--- crow/crow/monitor/dashboard.py
+++ crow/crow/monitor/dashboard.py
@@ -7,24 +7,90 @@ import dash_core_components as dcc
 import dash_html_components as html
 from plotly import graph_objects as go
 import dash_bootstrap_components as dbc
-from crow.events import LOG_MESSAGE, GENERATED_BC_VARIANT, EXPLORATION_RESULT
+from crow.events import LOG_MESSAGE, GENERATED_BC_VARIANT, EXPLORATION_RESULT, BC_EXPLORATION_QUEUE, GENERATION_QUEUE, \
+    WAT_QUEUE, WASM_QUEUE
 from crow.monitor.logger import ERROR
 from crow.events.event_manager import Subscriber, subscriber_function, Publisher
 import traceback
 from crow.settings import config
 from crow.monitor import MONITOR_QUEUE_NAME
+from crow.storage import STORAGE_QUEUE_NAME
 from crow.utils import printProgressBar
 from zipfile import ZipFile
 import os
 import flask
 from shutil import make_archive
 from flask.helpers import send_file
+import pika
 
+
+class RabbitMonitor:
+
+    def __init__(self):
+        BROKER_PASS = os.getenv("BROKER_PASS", None)
+        BROKER_USER = os.getenv("BROKER_USER", None)
+
+        if BROKER_PASS and BROKER_USER:
+            credentials = pika.PlainCredentials(BROKER_USER, BROKER_PASS)
+            param = pika.ConnectionParameters(host=config["event"]["host"],
+                                              virtual_host="/",
+                                              port=config["event"].getint("port"),
+                                              credentials=credentials)
+        else:
+            param = pika.ConnectionParameters(host=config["event"]["host"],
+                                              port=config["event"].getint("port"))
+        self.connection = pika.BlockingConnection(param)
+        self.channel = self.connection.channel()
+
+    def __get_message_count(self, QUEUE_NAME):
+
+        queue = self.channel.queue_declare(
+            queue=QUEUE_NAME,passive=True
+        )
+        return queue.method.message_count
+
+    def __purge_queue(self, queue_name):
+
+        print(f"Purging {queue_name}")
+        self.channel.queue_purge(queue_name)
+
+    def purge_exploration_queue(self):
+        self.__purge_queue(BC_EXPLORATION_QUEUE)
+
+    def purge_generation_queue(self):
+        self.__purge_queue(GENERATION_QUEUE)
+
+    def purge_storage_queue(self):
+        self.__purge_queue(STORAGE_QUEUE_NAME)
+
+    def purge_wasm2wat_queue(self):
+        self.__purge_queue(WASM_QUEUE)
+
+    def purge_wat2wasm_queue(self):
+        self.__purge_queue(WAT_QUEUE)
+
+    def get_exploration_message_count(self):
+        return self.__get_message_count(BC_EXPLORATION_QUEUE)
+
+    def get_generation_message_count(self):
+        return self.__get_message_count(GENERATION_QUEUE)
+
+    def get_storage_message_count(self):
+        return self.__get_message_count(STORAGE_QUEUE_NAME)
+
+    def get_wasm2wat_message_count(self):
+        return self.__get_message_count(WAT_QUEUE)
+
+    def get_bc2wasm_message_count(self):
+        return self.__get_message_count(WASM_QUEUE)
 
 PROGRAM_RE = re.compile(r"^(.+?):")
 BASE_DIR = os.path.dirname(__file__)
 
 if __name__ == "__main__":
+
+    m = RabbitMonitor()
+    m.get_exploration_message_count()
 
     redis_host = sys.argv[1]
     redis_port = int(sys.argv[2])
@@ -33,7 +99,7 @@ if __name__ == "__main__":
     out_folder = sys.argv[5]
 
     connection = redis.Redis(host=redis_host,port=redis_port, db=redis_db, password=redis_pass)
-    app = dash.Dash(external_stylesheets=[dbc.themes.LUX])
+    app = dash.Dash(external_stylesheets=[dbc.themes.LUX], suppress_callback_exceptions=True)
 
 
     def get_all_programs():
@@ -126,54 +192,95 @@ if __name__ == "__main__":
         )
 
 
+    @app.callback(dash.dependencies.Output('logs', 'children'),
+                  [dash.dependencies.Input('purge-button1', 'n_clicks'),
+                   dash.dependencies.Input('purge-button1', 'key')],
+                  )
+    def purge_exploration(n_clicks, key):
+
+        if n_clicks and key == "EXPLORATION":
+            return m.purge_exploration_queue()
+
+
+        return None
+
+
+
     @app.callback(dash.dependencies.Output('page-content', 'children'),
                   [dash.dependencies.Input('url', 'pathname'),
                     dash.dependencies.Input('update-button', 'n_clicks')],
                   )
     def display_page(pathname, n_clicks):
 
+
         def plot_general_stats():
 
-            programs = list(get_all_programs().keys())
+            return html.Div(
+                style=dict(
+                    padding="30px"
+                ),
+                 children=[
+                     html.Hr(),
+                     html.H2(
+                         children="Broker status, pending messages"
+                     ),
+                     html.A(
+                         style=dict(
+                             marginLeft='5px'
+                         ),
+                         id="purge-button1",
+                         className='btn btn-danger',
+                         children=f"Purge",
+                         key="EXPLORATION"
+                     ),
+                     html.Div(
+                    className="row",
+                    children=[
+                        html.Div(
+                            className='col',
+                            children=[
+                                html.A(
+                                    children=f"Exploration queue count: {m.get_exploration_message_count()}"
+                                )
 
-            x = programs
-            y = [ len(get_unique_bitcodes(p)) for p in programs]
-            t = [ get_total_llvm(p) for p in programs]
-
-            return None
-            '''dcc.Graph(
-                            id=f'GeneralGraph',
-                            figure={
-                                'data': [
-
-                                    go.Bar(
-                                        name="Unique",
-                                        x=x,
-                                        y=y,
-                                        offsetgroup=0
-                                    ),
-                                    go.Bar(
-                                        name="Total",
-                                        x=x,
-                                        y=t,
-                                        offsetgroup=0
-                                    )
-                                    #{'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': 'WASM'},
-                                ],
-                                'layout': {
-                                    'plot_bgcolor': colors['background'],
-                                    'paper_bgcolor': colors['background'],
-                                    'font': {
-                                        'color': colors['text']
-                                    },
-                                    'barmode': 'stack'
-                                }
-                            },
-                            style={
-                                'marginBottom': '60px'
-                            }
+                            ]
+                        ),
+                        html.Div(
+                            className='col',
+                            children=[
+                                html.A(
+                                    children=f"Generation queue count: {m.get_generation_message_count()}"
+                                ),
+                            ]
+                        ),
+                        html.Div(
+                            className='col',
+                            children=[
+                                html.A(
+                                    children=f"Storage queue count: {m.get_storage_message_count()}"
+                                ),
+                            ]
+                        ),
+                        html.Div(
+                            className='col',
+                            children=[
+                                html.A(
+                                    children=f"Wasm2wat queue count: {m.get_wasm2wat_message_count()}"
+                                ),
+                            ]
+                        ),
+                        html.Div(
+                            className='col',
+                            children=[
+                                html.A(
+                                    children=f"Bc2wasm queue count: {m.get_bc2wasm_message_count()}"
+                                ),
+                            ]
                         )
-            '''
+                    ]
+                ),
+                 html.Hr()]
+            )
 
         def generate_report_for_module(module):
 
@@ -185,9 +292,9 @@ if __name__ == "__main__":
             total_wasm = get_total_wasm(module)
             total_wat = get_total_wat(module)
 
-            '''
-            
-            '''
+            if total_llvm == 0:
+                return None
+
             return html.Div(
                 children=html.Div(
                     children=[
@@ -255,11 +362,16 @@ if __name__ == "__main__":
                         html.H2(
                             children=f"DB {redis_db}"
                         ),
-                        html.H2(
-                            children=f"{len(programs)} Modules "
-                        ),
                         plot_general_stats()
                     ],
+                    style={
+                        'textAlign': 'center',
+                        'color': colors['text']
+                    }
+                ),
+
+                html.H2(
+                    children=f"{len(programs)} Modules ",
                     style={
                         'textAlign': 'center',
                         'color': colors['text']
@@ -286,12 +398,10 @@ if __name__ == "__main__":
                     'marginRight': '50px'
                 }
             ),
+            html.Div(id='logs'),
             html.Div(id='page-content'),
         ])
-        '''
-        
-    
-        '''
+
         app.run_server(debug=debug)
 
     def print_general_stats(self):
@@ -317,5 +427,6 @@ if __name__ == "__main__":
     
     #subscriber = Subscriber(1, MONITOR_QUEUE_NAME, GENERATED_BC_KEY, config["event"].getint("port"), general_log)
     #subscriber.setup()
+
 
     deploy_dash()
