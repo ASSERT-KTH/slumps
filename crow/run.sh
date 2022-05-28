1818f33ab7365539990a76830be006d4da93f33e
Jacarte <javierca@kth.se>
2022-05-28 13:53:46 +0200

    Bash entrypoint for CROW

--- crow/run.sh
+++ crow/run.sh
@@ -1,4 +1,11 @@
-redis-cli -p 9090 config set requirepass $REDIS_PASS
-redis-cli -p 9090 config rewrite
+rabbitmqctl add_user $BROKER_USER $BROKER_PASS 
+rabbitmqctl set_user_tags $BROKER_USER administrator
+rabbitmqctl set_permissions -p / $BROKER_USER ".*" ".*" ".*"
+
+seep 2
+
+redis-server --port 1010 --requirepass $REDIS_PASS  &
+
+sleep 5
 
 python3 run.py $@
