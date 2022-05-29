6b00b2e99adc7095cac93362e67f7112424b341a
Jacarte <javierca@kth.se>
2022-05-29 09:43:05 +0200

    WIP

--- build_all_images.sh
+++ build_all_images.sh
@@ -1,17 +1,35 @@
 cd souper
 
-docker build -t slumps/souper:latest -f Dockerfile . 
+docker build -t slumps/souper:latest -f Dockerfile .
+
+docker push slumps/souper:latest
+
+curl --data parse_mode=Markdown --data chat_id="665043934" --data "text=Sper docker build and up " "https://api.telegram.org/bot$BOT_API_TOKEN/sendMessage"
 
 cd ..
 
 
 cd docker_images
 
+
 docker build -t slumps/backend:latest -f Dockerfile .
+
+docker push slumps/backend:latest
+
+curl --data parse_mode=Markdown --data chat_id="665043934" --data "text=Backend build and up" "https://api.telegram.org/bot$BOT_API_TOKEN/sendMessage"
 
 cd ..
 
 cd crow
 
-docker build -t slumps/crow2:standalone  -f Dockerfile .
+
+docker build -t slumps/crow2:standalone -f Dockerfile .
+docker push slumps/crow2:standalone
+
+
 docker build -t slumps/crow2:latest -f Dockerfile .
+docker push slumps/crow2:latest
+
+
+
+curl --data parse_mode=Markdown --data chat_id="665043934" --data "text=CROW built and up" "https://api.telegram.org/bot$BOT_API_TOKEN/sendMessage"
